b4e6ec522548da6e56605c1670ee16e6d4c4b62c
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-06 10:16:29 +0530

    Create 131-PalindromePartitioning

---
+++ 131-PalindromePartitioning/131-PalindromePartitioning.java
@@ -0,0 +1,40 @@
+// Last updated: 8/6/2025, 10:16:27 AM
+class Solution {
+    public List<List<String>> partition(String ques) {
+        // String ques = "nitin";
+		List<List<String>> ans = new ArrayList<>();
+		List<String> ll = new ArrayList<>();
+		Partitioning(ques, ll, ans);
+		return ans;
+
+	}
+
+	public static void Partitioning(String ques, List<String> ll, List<List<String>> ans) {
+		if (ques.length() == 0) {
+//			System.out.println(ll);
+			ans.add(new ArrayList<String>(ll));
+			return;
+		}
+		for (int cut = 1; cut <= ques.length(); cut++) {
+			String s = ques.substring(0, cut);
+			if (isPalindromic(s)) {
+				ll.add(s);
+				Partitioning(ques.substring(cut), ll, ans);
+				ll.remove(ll.size() - 1);
+			}
+		}
+	}
+
+	public static boolean isPalindromic(String s) {
+		int i = 0;
+		int j = s.length() - 1;
+		while (i < j) {
+			if (s.charAt(i) != s.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+		return true;
+    }
+}
