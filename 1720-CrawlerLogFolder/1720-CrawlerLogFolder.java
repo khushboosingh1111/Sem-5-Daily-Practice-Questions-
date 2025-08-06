c2fa56956fdf8a7e7a11160e1fbb8749ade0cdba
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-06 10:10:58 +0530

    Create 1720-CrawlerLogFolder

---
+++ 1720-CrawlerLogFolder/1720-CrawlerLogFolder.java
@@ -0,0 +1,31 @@
+// Last updated: 8/6/2025, 10:10:56 AM
+class Solution {
+    public int minOperations(String[] logs) {
+        /*Stack<String> paths_stack = new Stack<>();
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (!paths_stack.isEmpty()) {
+                    paths_stack.pop();
+                }
+            } else if (!log.equals("./")) {
+                paths_stack.push(log);
+            }
+        }
+
+        return paths_stack.size();*/
+        int res = 0;
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (res > 0) {
+                    res--;
+                }
+            } else if (!log.equals("./")) {
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
