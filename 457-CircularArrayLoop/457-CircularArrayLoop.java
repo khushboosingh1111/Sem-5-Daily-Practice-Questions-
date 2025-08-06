d5c643e66c328650240caf035da9e37b30c5029a
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-06 10:14:11 +0530

    Create 457-CircularArrayLoop

---
+++ 457-CircularArrayLoop/457-CircularArrayLoop.java
@@ -0,0 +1,67 @@
+// Last updated: 8/6/2025, 10:14:09 AM
+class Solution {
+
+    public boolean circularArrayLoop(int[] nums) {
+
+        int n = nums.length;
+
+
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] == 0) continue;
+
+
+            int slow = i, fast = nextIndex(nums, i);
+
+            
+
+            while (nums[i] * nums[fast] > 0 && nums[i] * nums[nextIndex(nums, fast)] > 0) {
+
+                if (slow == fast) {
+
+                 
+
+                    if (slow == nextIndex(nums, slow)) break;
+
+                    return true;
+
+                }
+
+                slow = nextIndex(nums, slow);
+
+                fast = nextIndex(nums, nextIndex(nums, fast));
+
+            }
+
+
+            int val = nums[i];
+
+            int idx = i;
+
+            while (val * nums[idx] > 0) {
+
+                int next = nextIndex(nums, idx);
+
+                nums[idx] = 0;
+
+                idx = next;
+
+            }
+
+        }
+
+
+        return false;
+
+    }
+
+
+    private int nextIndex(int[] nums, int i) {
+
+        int n = nums.length;
+
+        return ((i + nums[i]) % n + n) % n;
+
+    }
+
+}
