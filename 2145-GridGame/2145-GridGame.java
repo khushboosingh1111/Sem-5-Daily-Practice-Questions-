81e163802dc0d1263fe30bddc63e982e77246c86
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-06 10:10:26 +0530

    Create 2145-GridGame

---
+++ 2145-GridGame/2145-GridGame.java
@@ -0,0 +1,20 @@
+// Last updated: 8/6/2025, 10:10:24 AM
+class Solution {
+    public long gridGame(int[][] grid) {
+        return MaxPoints(grid);
+    }
+    public static long MaxPoints(int[][]grid){
+        long top_sum = 0;
+        for(int i=grid[0].length-1;i>=0;i--) {
+        	top_sum+=grid[0][i];
+        }
+        long b_sum = 0;
+        long min_sum=Long.MAX_VALUE;
+        for(int i = 0;i<grid[0].length;i++) {
+        	top_sum-=grid[0][i];
+        	min_sum = Math.min(min_sum, Math.max(top_sum, b_sum));
+        	b_sum+=grid[1][i];
+        }
+        return min_sum;
+	}
+}
