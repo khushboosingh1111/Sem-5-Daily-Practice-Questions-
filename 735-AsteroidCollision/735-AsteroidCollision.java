a68a233c9a7a48986926e64065b1c73de00d0329
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-06 10:13:09 +0530

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -0,0 +1,27 @@
+// Last updated: 8/6/2025, 10:13:07 AM
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack= new Stack<>();
+        for(int asteroid:asteroids){
+            if(asteroid>0){
+                stack.push(asteroid);
+                continue;
+            }
+            while(!stack.isEmpty() && stack.peek()>0 && Math.abs(asteroid)>stack.peek()){
+                stack.pop();
+            }
+            if(stack.isEmpty() || stack.peek()<0){
+                stack.push(asteroid);
+            }
+            else if(stack.peek()==Math.abs(asteroid)){
+                stack.pop();
+            }
+
+        }
+        int[] ans=new int[stack.size()];
+        for(int i=ans.length-1;i>=0;i--){
+            ans[i]=stack.pop();
+        }
+        return ans;
+    }
+}
