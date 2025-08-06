c21b66a22b8bec2a38e05da2131bceece06b2b0a
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-06 10:09:17 +0530

    Create 3723-SumOfGoodNumbers

---
+++ 3723-SumOfGoodNumbers/3723-SumOfGoodNumbers.java
@@ -0,0 +1,19 @@
+// Last updated: 8/6/2025, 10:09:14 AM
+class Solution {
+    public int sumOfGoodNumbers(int[] nums, int k) {
+        return sum(nums, k);
+    }
+    public static int sum(int[] arr, int k){
+        int ans = 0;
+        for(int i=0;i<arr.length;i++){
+           
+            if(i-k>= 0 && arr[i]<=arr[i-k]){
+                continue;
+            }else if(i+k<arr.length && arr[i]<=arr[i+k]){
+                continue;
+            }
+            ans+=arr[i];
+        }
+        return ans;
+    }
+}
