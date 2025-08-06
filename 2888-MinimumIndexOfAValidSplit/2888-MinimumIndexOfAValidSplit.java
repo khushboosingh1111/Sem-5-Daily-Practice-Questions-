5afe04d3a43ee8da2288703c1edb88f13a024f82
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-06 10:10:01 +0530

    Create 2888-MinimumIndexOfAValidSplit

---
+++ 2888-MinimumIndexOfAValidSplit/2888-MinimumIndexOfAValidSplit.java
@@ -0,0 +1,37 @@
+// Last updated: 8/6/2025, 10:09:59 AM
+class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        if(nums.size()==2)return 0;
+        else if(nums.size()==1)return -1;
+        return validSplit(nums);
+    }
+    public static int validSplit(List<Integer> arr) {
+		int element = 0;
+		int vote = 1;
+		for (int i = 0; i < arr.size(); i++) {
+			if(arr.get(i)==element) {
+				vote++;
+			}else {
+				vote--;
+				if(vote==0) {
+					element=arr.get(i);
+					vote=1;
+				}
+			}
+		}
+		int total = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if(arr.get(i)==element)total++;
+		}
+       int count=0;
+		int result = -1;
+		for (int i = 0; i < arr.size()-1; i++) {
+			if(arr.get(i)==element)count++;                    
+			if(count==(i+1)/2+1 && total-count>(arr.size()-i-1)/2) {
+				result=i;
+				break;
+			}
+		}
+		return result;
+	}
+}
