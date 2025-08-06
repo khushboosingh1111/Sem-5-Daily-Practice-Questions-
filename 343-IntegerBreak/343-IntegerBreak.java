501375df1f46354f08f5507677716531fe861b0d
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-06 10:14:59 +0530

    Create 343-IntegerBreak

---
+++ 343-IntegerBreak/343-IntegerBreak.java
@@ -0,0 +1,15 @@
+// Last updated: 8/6/2025, 10:14:57 AM
+public class Solution {
+    public int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+        int prod = 1;
+        while(n>4){
+            prod*=3;
+            n-=3;
+        }
+        prod*=n;
+        
+        return prod;
+    }
+}
