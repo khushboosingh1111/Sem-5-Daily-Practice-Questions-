3725285e2bd592e9d071760a86ae883c0d39708b
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-06 10:12:48 +0530

    Create 852-FriendsOfAppropriateAges

---
+++ 852-FriendsOfAppropriateAges/852-FriendsOfAppropriateAges.java
@@ -0,0 +1,22 @@
+// Last updated: 8/6/2025, 10:12:46 AM
+class Solution {
+    public int numFriendRequests(int[] ages) {
+        int[] freq = new int[121];
+        for (int age : ages) {
+            freq[age]++;
+        }
+        int req = 0;
+        for (int a = 15; a <= 120; a++) {
+            if (freq[a] == 0) continue;
+            for (int b = (int)(0.5 * a + 8); b <= a; b++) {
+                if (freq[b] == 0) continue;
+                if (a == b) {
+                    req += freq[a] * (freq[a] - 1);
+                } else {
+                    req += freq[a] * freq[b];
+                }
+            }
+        }
+        return req;
+    }
+}
