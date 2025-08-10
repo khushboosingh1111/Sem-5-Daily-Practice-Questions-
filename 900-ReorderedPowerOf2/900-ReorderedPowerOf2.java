1a5dcbcfbe726f946c86358073d57b125117562b
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-10 22:34:23 +0530

    Create 900-ReorderedPowerOf2

---
+++ 900-ReorderedPowerOf2/900-ReorderedPowerOf2.java
@@ -0,0 +1,16 @@
+// Last updated: 8/10/2025, 10:34:23 PM
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        String s = sortDigits(n);
+        for (int i = 1; i <= 1_000_000_000; i <<= 1) {
+            if (sortDigits(i).equals(s)) return true;
+        }
+        return false;
+    }
+
+    private String sortDigits(int x) {
+        char[] digits = String.valueOf(x).toCharArray();
+        Arrays.sort(digits);
+        return new String(digits);
+    }
+}
