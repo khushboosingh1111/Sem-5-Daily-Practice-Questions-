acc65b98f0644d9da2459a7d3d03291cdfbb63eb
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-08-31 22:55:58 +0530

    Create 538-ConvertBSTtoGreaterTree

---
+++ 538-ConvertBSTtoGreaterTree/538-ConvertBSTtoGreaterTree.java
@@ -0,0 +1,33 @@
+// Last updated: 8/31/2025, 10:55:57 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+    TreeNode dummy = root;
+        int a = change(root,0);
+        return dummy;
+    }
+    public int change(TreeNode root, int sum){
+        if(root==null){
+            return sum;
+        }
+        int a = change(root.right,sum);
+        root.val +=a;
+        int b = change(root.left,root.val);
+        return b;
+    }
+    
+}
