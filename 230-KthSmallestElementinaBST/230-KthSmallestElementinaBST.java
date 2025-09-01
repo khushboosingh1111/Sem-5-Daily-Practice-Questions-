1cb23e7c728c15a335ad5c70b800ba6a36243b37
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-09-01 20:36:51 +0530

    Create 230-KthSmallestElementinaBST

---
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.java
@@ -0,0 +1,36 @@
+// Last updated: 9/1/2025, 8:36:47 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int ans=-1;
+    private int count=0;
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root,k);
+        return ans;
+    }
+    private void inorder(TreeNode root,int k){
+        if(root==null){
+            return;
+        }
+        inorder(root.left,k);
+        count++;
+        if(count==k){
+            ans=root.val;
+            return;
+        }
+        inorder(root.right,k);
+    }
+}
