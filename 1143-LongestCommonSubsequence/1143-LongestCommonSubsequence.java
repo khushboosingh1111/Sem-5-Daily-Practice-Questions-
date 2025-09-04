8c57148e90458263ad1027fb0044fc7e0a61479b
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-09-04 12:29:05 +0530

    File updated at 9/4/2025, 12:29:00 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,22 +1,38 @@
-// Last updated: 9/4/2025, 11:17:43 AM
+// Last updated: 9/4/2025, 12:29:00 PM
 class Solution {
-    public int maxUncrossedLines(int[] s1, int[] s2) {
-        int[][] dp=new int[s1.length+1][s2.length+1];
-		for(int i=1;i<dp.length;i++) {
-			for(int j=1;j<dp[0].length;j++) {
-				int ans=0;
-				if(s1[i-1]==s2[j-1]) {
-					ans=1+dp[i-1][j-1];
-				}
-				else {
-					int f=dp[i-1][j];
-					int s=dp[i][j-1];
-					ans=Math.max(f, s);
-				}
-				dp[i][j]=ans;
-			}
-		}
-		return dp[dp.length-1][dp[0].length-1];
-	
+    public int numDistinct(String s, String t) {
+        // dp array initialize
+        int[][] dp = new int[s.length()][t.length()];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return distinctSubseq(s, t, 0, 0, dp);
+    }
+
+    // recursion + memoization
+    public static int distinctSubseq(String s, String t, int i, int j, int[][] dp) {
+        // agar t pura match ho gaya
+        if (j == t.length()) {
+            return 1;
+        }
+        // agar s pura traverse ho gaya par t abhi bacha hai
+        if (i == s.length()) {
+            return 0;
+        }
+        // agar already computed hai
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int inc = 0, exc = 0;
+        // match case
+        if (s.charAt(i) == t.charAt(j)) {
+            inc = distinctSubseq(s, t, i + 1, j + 1, dp);
+        }
+        // exclude case
+        exc = distinctSubseq(s, t, i + 1, j, dp);
+
+        return dp[i][j] = inc + exc;
     }
 }
