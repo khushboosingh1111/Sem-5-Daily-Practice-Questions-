fc635bb86d14ca53e3a7e04cc223be244beb8bf0
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-09-10 12:37:02 +0530

    File updated at 9/10/2025, 12:37:02 PM

--- 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
+++ 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
@@ -1,6 +1,7 @@
-// Last updated: 9/10/2025, 12:22:32 PM
+// Last updated: 9/10/2025, 12:37:02 PM
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
+      
         // int[][] matrix = {{2, 1, 3}, {6, 5, 4}, {7, 8, 9}};
         int ans = Integer.MAX_VALUE;
         int[][] dp=new int[matrix.length][matrix[0].length];
@@ -23,11 +24,16 @@ class Solution {
         if(dp[cr][cc]!=-66666) {
         	return dp[cr][cc];
         }
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<matrix[0].length;i++) {
+        	if(i==cc) {
+        		continue;
+        	}
+        	ans=Math.min(ans,Minimum_Falling_Path(matrix,cr+1,i,dp));
+        }
 
-        int ld = Minimum_Falling_Path(matrix, cr + 1, cc - 1,dp);
-        int rd = Minimum_Falling_Path(matrix, cr + 1, cc + 1,dp);
-        int d = Minimum_Falling_Path(matrix, cr + 1, cc,dp);
+        
 
-        return dp[cr][cc]=Math.min(d, Math.min(ld, rd)) + matrix[cr][cc];
+        return dp[cr][cc]=ans+matrix[cr][cc];
     }
 }
