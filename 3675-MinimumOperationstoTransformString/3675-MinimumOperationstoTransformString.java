59ed5d81a89ef3ad5d83ffef5169d70668aed759
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-09-13 19:57:06 +0530

    File updated at 9/13/2025, 7:57:05 PM

--- 3675-MinimumOperationstoTransformString/3675-MinimumOperationstoTransformString.java
+++ 3675-MinimumOperationstoTransformString/3675-MinimumOperationstoTransformString.java
@@ -1,14 +1,41 @@
-// Last updated: 9/13/2025, 7:50:24 PM
+// Last updated: 9/13/2025, 7:57:05 PM
+/*class Solution {
+    public int minOperations(String s) {
+        int ans=0;
+        TreeMap<Character,Integer>charMap=new TreeMap<>((a,b)->a-b);
+        for(char ch:s.toCharArray()){
+            charMap.put(ch,charMap.getOrDefault(ch,0)+1);
+        }
+        while(charMap.size()>0){
+            char ch=charMap.firstKey();
+            int freq=charMap.get(ch);
+            if(ch=='a'){
+                charMap.remove(ch);
+            }else{
+                int nextIdx=((ch-'a')+1)%26;
+                char nextChar=(char)('a'+ nextIdx);
+                charMap.put(nextChar,charMap.getOrDefault(nextChar,0)+freq);
+                charMap.remove(ch);
+                ans++;
+            }
+        }
+        return ans;
+    }
+}*/
+
 class Solution {
     public int minOperations(String s) {
-        int maxSteps = 0;
-
-        for (char ch : s.toCharArray()) {
-            int steps = ('a' - ch + 26) % 26;
-            maxSteps = Math.max(maxSteps, steps);
+        int ans=0;
+        int[]freq=new int[26];
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
         }
-        return maxSteps;
-        
-        
+        for(int i=1;i<26;i++){
+            if(freq[i]!=0){
+                ans++;
+                freq[(i+1)%26]+=freq[i];
+            }
+        }
+        return ans;
     }
 }
