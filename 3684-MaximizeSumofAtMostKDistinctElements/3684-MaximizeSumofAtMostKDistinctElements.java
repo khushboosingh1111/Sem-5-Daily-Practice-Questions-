4e4f67a74b70f70a3d6e318a761f16f584364e11
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-09-14 13:59:27 +0530

    Create 3684-MaximizeSumofAtMostKDistinctElements

---
+++ 3684-MaximizeSumofAtMostKDistinctElements/3684-MaximizeSumofAtMostKDistinctElements.java
@@ -0,0 +1,18 @@
+// Last updated: 9/14/2025, 1:59:24 PM
+class Solution {
+    public int[] maxKDistinct(int[] nums, int k) {
+        // int[] arr=nums;
+        Set<Integer> set=new HashSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+        List<Integer> ans=new ArrayList<>(set);
+        ans.sort(Collections.reverseOrder());
+        int n=Math.min(k,ans.size());
+        int[] res=new int[n];
+        for(int i=0;i<n;i++){
+            res[i]=ans.get(i);
+        }
+        return res;
+    }
+}
