09de7f13330ab0c8fdf4b80013b3a4d84697e990
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-10-06 23:08:39 +0530

    Create

---
+++ problem-1759772312012/problem-1759772312012.java
@@ -0,0 +1,7 @@
+// Last updated: 10/6/2025, 11:08:32 PM
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        int maxPowerOf3 = 1162261467; // 3^19 is the largest power of 3 in int range
+        return n > 0 && maxPowerOf3 % n == 0;
+    }
+}
