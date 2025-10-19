24f6054be1759db77b759f8cb226ab31b63b91bb
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-10-19 19:05:20 +0530

    Create

---
+++ problem-1760880918332/problem-1760880918332.java
@@ -0,0 +1,24 @@
+// Last updated: 10/19/2025, 7:05:18 PM
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int st=1;
+        int end=num/2;
+        if(num==1){
+            return true;
+        }
+        while(st<=end){
+            long mid=st+(end-st)/2;
+            long sq=mid*mid;
+            if(sq==num){
+                return true;
+            }
+            else if(sq>num){
+                end=(int)mid-1;
+            }
+            else{
+                st=(int)mid+1;
+            }
+        }
+        return false;
+    }
+}
