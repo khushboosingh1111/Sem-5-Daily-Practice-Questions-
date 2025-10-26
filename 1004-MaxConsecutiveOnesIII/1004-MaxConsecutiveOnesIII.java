4741df8be5a47ab0a2ffeb2ba8a286e22f164b85
khushboosingh1111 <khushboo2004.singh@gmail.com>
2025-10-26 22:12:54 +0530

    Create 1004-MaxConsecutiveOnesIII

---
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -0,0 +1,21 @@
+// Last updated: 10/26/2025, 10:12:52 PM
+class Solution {
+    public int longestOnes(int[] s, int k) {
+        int[] count = new int[2];
+        int maxCount = 0, left = 0, ans = 0;
+
+        for (int right = 0; right < s.length; right++) {
+            count[s[right]]++;
+            maxCount = Math.max(maxCount, count[1]);
+
+            while ((right - left + 1) - maxCount > k) {
+                count[s[left]]--;
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+}
