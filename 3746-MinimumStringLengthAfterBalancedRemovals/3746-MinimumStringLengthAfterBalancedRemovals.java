7d413617a2149f933e020c3d14cc43b965ca522a
Khushboo Singh <khushboo2004.singh@gmail.com>
2025-11-17 18:34:02 +0530

    Create 3746-MinimumStringLengthAfterBalancedRemovals

---
+++ 3746-MinimumStringLengthAfterBalancedRemovals/3746-MinimumStringLengthAfterBalancedRemovals.java
@@ -0,0 +1,16 @@
+// Last updated: 11/17/2025, 6:33:58 PM
+class Solution {
+    public int minLengthAfterRemovals(String s) {
+        int ca=0;
+        int cb=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='a'){
+                ca++;
+            }
+            else if(ch=='b'){
+                cb++;
+            }
+        }
+        return Math.abs(ca-cb);
+    }
+}
