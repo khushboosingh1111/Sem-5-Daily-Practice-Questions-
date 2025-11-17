bf65c2cd00c36b72656349d929e63bd3cc4f2ffe
Khushboo Singh <khushboo2004.singh@gmail.com>
2025-11-17 18:32:21 +0530

    Create 3745-MaximizeExpressionofThreeElements

---
+++ 3745-MaximizeExpressionofThreeElements/3745-MaximizeExpressionofThreeElements.java
@@ -0,0 +1,7 @@
+// Last updated: 11/17/2025, 6:32:17 PM
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length-1]+nums[nums.length-2]-nums[0];
+    }
+}
