38519144433b1fb0bc355983ef422ebe41e94237
Khushboo Singh <khushboo2004.singh@gmail.com>
2025-11-19 12:35:59 +0530

    Create 2360-LongestCycleinaGraph

---
+++ 2360-LongestCycleinaGraph/2360-LongestCycleinaGraph.java
@@ -0,0 +1,50 @@
+// Last updated: 11/19/2025, 12:35:58 PM
+class Solution {
+    public int longestCycle1(int[] edges) {
+        return longestCycle(edges);
+    }
+    public int longestCycle(int[] edges) {
+        int[] in=new int[edges.length];
+        for(int i=0;i<edges.length;i++) {
+        	if(edges[i]!=-1) {
+        		in[edges[i]]++;        		
+        	}
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<in.length;i++) {
+        	if(in[i]==0) {
+        		q.add(i);
+        	}
+        }
+        boolean[] visited=new boolean[edges.length];
+        while(!q.isEmpty()) {
+        	int e=q.poll();
+        	visited[e]=true;
+        	if(edges[e]!=-1) {
+        		in[edges[e]]--;
+            	if(in[edges[e]]==0) {
+            		q.add(edges[e]);
+            	}
+        	}
+        	
+        }
+        int ans=-1; //agr cycle nhi mila toh -1
+        for(int i=0;i<visited.length;i++) { //i=0 k liye jitne vtx pr kaam kr chuke h i=1 k liye utne kaam nhi hoga
+        	if(visited[i]==false) { //jo vtx false hoga ussi pr kaam krenge bss  
+        		int c=1; //count for max vertexs for longest cycle
+    			visited[i]=true;
+        		int nbrs=edges[i]; //visited[i] vale ka nbrs kon h
+        		while(nbrs!=i) { //jha nbrs h vhi laut kr aa gye vapas mtlb cycle h
+        			c++;
+        			visited[nbrs]=true;
+        			nbrs=edges[nbrs];
+        			
+        		}
+        		ans=Math.max(ans,c);
+        	}
+        }
+        return ans;
+    }
+
+}
+
