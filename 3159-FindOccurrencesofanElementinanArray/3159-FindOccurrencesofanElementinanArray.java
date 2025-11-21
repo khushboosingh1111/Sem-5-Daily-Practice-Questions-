7af59aeb4bc45af9199e6708f597b2769bb2db64
Khushboo Singh <khushboo2004.singh@gmail.com>
2025-11-21 15:39:16 +0530

    Create 3159-FindOccurrencesofanElementinanArray

---
+++ 3159-FindOccurrencesofanElementinanArray/3159-FindOccurrencesofanElementinanArray.java
@@ -0,0 +1,21 @@
+// Last updated: 11/21/2025, 3:39:14 PM
+class Solution {
+    public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
+       HashMap<Integer,Integer> map=new HashMap<>();
+       int occ=1;
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]==x){
+            map.put(occ++,i);
+        }
+       }
+
+       int [] ans= new int[queries.length];
+       
+       for( int i = 0; i < queries.length;i++){
+        int q = queries[i];
+        ans[i]=map.getOrDefault(q,-1);
+       }
+       return ans;
+       
+    }
+}
