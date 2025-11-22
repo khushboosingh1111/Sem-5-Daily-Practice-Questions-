7e1728175681376abfd60c032d4b201f1140e82d
Khushboo Singh <khushboo2004.singh@gmail.com>
2025-11-22 20:38:48 +0530

    Create

---
+++ problem-1763824124543/problem-1763824124543.java
@@ -0,0 +1,26 @@
+// Last updated: 11/22/2025, 8:38:44 PM
+class Solution {
+    public int totalWaviness(int num1, int num2) {
+        
+        int ans=0;
+        for(int i=num1;i<=num2;i++){
+            String s=Integer.toString(i);
+            if(s.length()<3){
+                continue;
+            }
+            int[] arr=new int[s.length()];
+            for(int j=0;j<s.length();j++){
+                arr[j]=s.charAt(j)-'0';
+            }
+            for(int k=1;k+1<arr.length;k++){
+                int a=arr[k-1];
+                int b=arr[k];
+                int c=arr[k+1];
+                if((a<b && b>c) || (a>b && b<c)){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
