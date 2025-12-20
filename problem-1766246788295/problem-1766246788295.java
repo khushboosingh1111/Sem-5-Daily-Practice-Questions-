9a6d05dc6e44f9223d1abbc7a19a52c71aefe382
Khushboo Singh <khushboo2004.singh@gmail.com>
2025-12-20 21:36:29 +0530

    Create

---
+++ problem-1766246788295/problem-1766246788295.java
@@ -0,0 +1,17 @@
+// Last updated: 12/20/2025, 9:36:28 PM
+1class Solution {
+2    public long lastInteger(long n) {
+3        long start = 1;
+4        long gap = 1;
+5        boolean left = true;
+6        while(n>1){
+7            if(!left && (n%2==0)){
+8                start+=gap;
+9            }
+10            gap<<=1;
+11            n= (n+1)>>1;
+12            left = !left;
+13        }
+14        return start;
+15    }
+16}
