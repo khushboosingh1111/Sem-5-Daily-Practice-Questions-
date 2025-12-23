7a9fec56b5f3c4f80a52b9686d673bd3225ac630
Khushboo Singh <khushboo2004.singh@gmail.com>
2025-12-23 15:50:56 +0530

    Create 3781-MaximumScoreAfterBinarySwaps

---
+++ 3781-MaximumScoreAfterBinarySwaps/3781-MaximumScoreAfterBinarySwaps.java
@@ -0,0 +1,16 @@
+// Last updated: 12/23/2025, 3:50:53 PM
+1class Solution {
+2    public long maximumScore(int[] nums, String s) {
+3        //1 ko peeche left m move kr skte h
+4        long res=0;
+5        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+6        for(int i=0;i<nums.length;i++){
+7            pq.offer(nums[i]);
+8            if(s.charAt(i)=='1'){
+9                res+=pq.poll();
+10            }
+11        }
+12        return res;
+13        
+14    }
+15}
