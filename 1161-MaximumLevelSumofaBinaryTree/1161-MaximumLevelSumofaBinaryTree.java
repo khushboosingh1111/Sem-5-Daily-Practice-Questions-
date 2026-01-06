72310fbc33116d4f7cb1037959d0b7dd86ca9836
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-06 20:54:07 +0530

    File updated at 1/6/2026, 8:54:07 PM

--- 1161-MaximumLevelSumofaBinaryTree/1161-MaximumLevelSumofaBinaryTree.java
+++ 1161-MaximumLevelSumofaBinaryTree/1161-MaximumLevelSumofaBinaryTree.java
@@ -1,4 +1,4 @@
-// Last updated: 1/6/2026, 8:50:45 PM
+// Last updated: 1/6/2026, 8:54:07 PM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -16,33 +16,30 @@
 15 */
 16class Solution {
 17    public int maxLevelSum(TreeNode root) {
-18        Queue<TreeNode> q = new LinkedList<>();
-19        q.add(root);
-20
-21        int level = 1;
-22        int ansLevel = 1;
-23        int maxSum = Integer.MIN_VALUE;
-24
-25        while (!q.isEmpty()) {
-26            int size = q.size();
-27            int sum = 0;
-28
-29            for (int i = 0; i < size; i++) {
-30                TreeNode node = q.poll();
-31                sum += node.val;
-32
-33                if (node.left != null) q.add(node.left);
-34                if (node.right != null) q.add(node.right);
+18        Queue<TreeNode> queue = new LinkedList<>();
+19        int max = root.val;
+20        queue.offer(root);
+21        int maxLevel = 1;
+22        int currentLevel = 1;
+23        while(!queue.isEmpty()){
+24            int queueSize = queue.size();
+25            int sum = 0;
+26            for(int i=0;i<queueSize;i++){
+27                TreeNode val = queue.poll();
+28                if(val.left!=null){
+29                    queue.offer(val.left);
+30                }
+31                if(val.right!=null){
+32                    queue.offer(val.right);
+33                }
+34                sum+= val.val;
 35            }
-36
-37            if (sum > maxSum) {
-38                maxSum = sum;
-39                ansLevel = level;
-40            }
-41
-42            level++;
-43        }
-44
-45        return ansLevel;
-46    }
-47}
+36            if(sum>max){
+37                max = sum;
+38                maxLevel = currentLevel;
+39            }
+40            currentLevel++;
+41        }
+42        return maxLevel;
+43    }
+44}
