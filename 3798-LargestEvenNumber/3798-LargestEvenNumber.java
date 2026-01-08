40d8ed80612f5df6d5f5c2e68aab9aa53b0066f3
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-08 22:15:16 +0530

    File updated at 1/8/2026, 10:15:14 PM

--- 3798-LargestEvenNumber/3798-LargestEvenNumber.java
+++ 3798-LargestEvenNumber/3798-LargestEvenNumber.java
@@ -1,15 +1,9 @@
-// Last updated: 1/8/2026, 10:14:31 PM
+// Last updated: 1/8/2026, 10:15:14 PM
 1class Solution {
 2    public String largestEven(String s) {
-3         int lastTwoIndex = s.lastIndexOf('2');
-4
-5        // If no '2' exists, cannot form even number
-6        if (lastTwoIndex == -1) {
-7            return "";
-8        }
-9
-10        // Keep everything up to last '2'
-11        return s.substring(0, lastTwoIndex + 1);
-12    }
-13}
-14  
+3        int idx = s.lastIndexOf('2');
+4        if(idx!=-1) return s.substring(0,idx+1);
+5        return "";
+6    }
+7}
+8  
