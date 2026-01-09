8c36f0bfe109e44d2b4ed65ca8fc738132eaa078
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-09 21:44:19 +0530

    Create 3794-ReverseStringPrefix

---
+++ 3794-ReverseStringPrefix/3794-ReverseStringPrefix.java
@@ -0,0 +1,15 @@
+// Last updated: 1/9/2026, 9:44:19 PM
+1class Solution {
+2    public String reversePrefix(String s, int k) {
+3        char[] temp = s.toCharArray();
+4        StringBuilder sb = new StringBuilder();
+5        for(int i = k - 1; i >= 0; i--){
+6            sb.append(temp[i]);
+7        }    
+8        for(int i = k; i < temp.length; i++){
+9            sb.append(temp[i]);
+10        }
+11        return sb.toString();
+12        
+13    }
+14}
