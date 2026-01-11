200eb0ad1e8793a4a53bba78eb4987a4ace2f05e
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-12 00:25:04 +0530

    File updated at 1/12/2026, 12:25:02 AM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,14 +1,16 @@
-// Last updated: 1/12/2026, 12:23:43 AM
+// Last updated: 1/12/2026, 12:25:02 AM
 1class Solution {
 2    public int uniquePaths(int m, int n) {
-3        int [] dp=new int[n];
-4        dp[0]=1;
-5        for(int i=0;i<m;i++){
-6            for(int j=1;j<n;j++){
-7                dp[j]+=dp[j-1];
-8            }
-9        }
-10        return dp[n-1];
+3        int N = m + n - 2;
+4    int k = Math.min(m - 1, n - 1);
+5
+6    long res = 1;
+7
+8    for (int i = 1; i <= k; i++) {
+9        res = res * (N - k + i) / i;
+10    }
 11
-12    }
-13}
+12    return (int) res;
+13
+14    }
+15}
