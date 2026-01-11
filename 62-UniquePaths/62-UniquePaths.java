50d9d9bb20647823dc9c289d3bade8cd0320e5bb
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-12 00:48:36 +0530

    File updated at 1/12/2026, 12:48:34 AM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,16 +1,16 @@
-// Last updated: 1/12/2026, 12:26:14 AM
+// Last updated: 1/12/2026, 12:48:34 AM
 1class Solution {
-2    public int uniquePaths(int m, int n) {
-3    int N = m + n - 2;
-4    int k = Math.min(m - 1, n - 1);
-5
-6    long res = 1;
-7
-8    for (int i = 1; i <= k; i++) {
-9        res = res * (N - k + i) / i;
-10    }
-11
-12    return (int) res;
-13
+2        public int minimumCoins(int[] A) {
+3        int n = A.length, dp[] = new int[n + 1];
+4        Deque<Integer> q = new ArrayDeque<>();
+5        for (int i = 0; i < n; i++) {
+6            while (!q.isEmpty() && (q.getFirst() + 1) * 2 < i + 1)
+7                q.removeFirst();
+8            while (!q.isEmpty() && dp[q.getLast()] + A[q.getLast()] >= dp[i] + A[i])
+9                q.removeLast();
+10            q.addLast(i);
+11            dp[i + 1] = dp[q.getFirst()] + A[q.getFirst()];
+12        }
+13        return dp[n];
 14    }
 15}
