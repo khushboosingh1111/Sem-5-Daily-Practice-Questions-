f4f3bcc7dbbebe017c41bf2e5a0aeb87d0a943d9
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-12 22:17:19 +0530

    Create 1266-MinimumTimeVisitingAllPoints

---
+++ 1266-MinimumTimeVisitingAllPoints/1266-MinimumTimeVisitingAllPoints.java
@@ -0,0 +1,13 @@
+// Last updated: 1/12/2026, 10:17:12 PM
+1class Solution {
+2    public int minTimeToVisitAllPoints(int[][] p){                                                                                   
+3        int ans = 0;
+4        for (int i = 1; i < p.length; i++) {
+5            ans += Math.max(
+6                Math.abs(p[i][0] - p[i - 1][0]),
+7                Math.abs(p[i][1] - p[i - 1][1])
+8            );
+9        }
+10        return ans;
+11    }
+12}                                                                                                                                                     
