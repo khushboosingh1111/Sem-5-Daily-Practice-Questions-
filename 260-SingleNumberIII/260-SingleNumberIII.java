03045b317731bf74f9cd11f119b7a47e26a9f45d
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-14 12:11:42 +0530

    Create 260-SingleNumberIII

---
+++ 260-SingleNumberIII/260-SingleNumberIII.java
@@ -0,0 +1,16 @@
+// Last updated: 1/14/2026, 12:11:42 PM
+1class Solution {
+2    public int[] singleNumber(int[] nums) {
+3        int [] ans = new int[2];
+4        int xor = 0;
+5        for(int i : nums)xor^=i;
+6
+7        int lowerbits = xor & -xor;
+8
+9        for(int i: nums){
+10            if((i & lowerbits) == 0) ans[0]^=i;
+11            else ans[1]^=i;
+12        }
+13        return ans;
+14    }
+15}
