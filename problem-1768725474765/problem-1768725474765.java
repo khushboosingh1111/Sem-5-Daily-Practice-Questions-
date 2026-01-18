64d27d774dc69b5886cf81c05d6f706a011866c4
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-18 14:07:56 +0530

    Create

---
+++ problem-1768725474765/problem-1768725474765.java
@@ -0,0 +1,14 @@
+// Last updated: 1/18/2026, 2:07:54 PM
+1class Solution {
+2    public int minOperations(int[] nums, int[] target) {
+3        HashSet<Integer> set=new HashSet<>();
+4        int n=nums.length;
+5        for(int i=0;i<n;i++){
+6            if(nums[i]==target[i]){
+7                continue;
+8            }
+9            set.add(nums[i]);
+10        }
+11        return set.size();
+12    }
+13}
