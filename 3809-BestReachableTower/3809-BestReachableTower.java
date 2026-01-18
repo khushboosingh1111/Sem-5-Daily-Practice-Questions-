849eaa618791240c020ea2f7682df03bf3dd0eba
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-18 14:08:57 +0530

    File updated at 1/18/2026, 2:08:56 PM

--- 3809-BestReachableTower/3809-BestReachableTower.java
+++ 3809-BestReachableTower/3809-BestReachableTower.java
@@ -1,14 +1,12 @@
-// Last updated: 1/18/2026, 2:08:21 PM
+// Last updated: 1/18/2026, 2:08:56 PM
 1class Solution {
 2    public int minOperations(int[] nums, int[] target) {
-3        HashSet<Integer> set=new HashSet<>();
-4        int n=nums.length;
-5        for(int i=0;i<n;i++){
-6            if(nums[i]==target[i]){
-7                continue;
-8            }
-9            set.add(nums[i]);
-10        }
-11        return set.size();
-12    }
-13}
+3        Set<Integer> s = new HashSet<>();
+4        for (int i = 0; i < nums.length; i++) {
+5            if (nums[i] != target[i]) {
+6                s.add(nums[i]);
+7            }
+8        }
+9        return s.size();
+10    }
+11}
