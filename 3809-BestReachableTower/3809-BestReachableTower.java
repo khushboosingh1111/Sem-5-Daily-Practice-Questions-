1559cc35a026b2c74ea5e0483bc730a48a47b01f
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-18 10:41:04 +0530

    File updated at 1/18/2026, 10:41:02 AM

--- 3809-BestReachableTower/3809-BestReachableTower.java
+++ 3809-BestReachableTower/3809-BestReachableTower.java
@@ -1,32 +1,27 @@
-// Last updated: 1/18/2026, 10:33:32 AM
+// Last updated: 1/18/2026, 10:41:02 AM
 1class Solution {
 2    public int[] bestTower(int[][] towers, int[] center, int radius) {
-3
-4        int bestQuality = -1;
-5        int bestX = -1, bestY = -1;
-6
-7        for (int[] t : towers) {
-8            int x = t[0];
-9            int y = t[1];
-10            int q = t[2];
-11
-12            int dist = Math.abs(x - center[0]) + Math.abs(y - center[1]);
-13
-14            if (dist <= radius) {
-15                //jiska jada quility ya quality equal hai aur lexicographically smaller hair to update krdo
-16                if (q > bestQuality ||
-17                   (q == bestQuality &&
-18                   (x < bestX || (x == bestX && y < bestY)))) {
-19
-20                    bestQuality = q;
-21                    bestX = x;
-22                    bestY = y;
-23                }
-24            }
-25        }
-26        //best quality radius k andr nhi mili toh
-27        if (bestQuality == -1) return new int[]{-1, -1};
-28        return new int[]{bestX, bestY};
-29    }
-30}
-31
+3        int bestQuality=-1;
+4        int bestx=-1;
+5        int besty=-1;
+6        for(int[] t:towers){
+7            int x=t[0];
+8            int y=t[1];
+9            int q=t[2];
+10            int dis=Math.abs(x-center[0])+Math.abs(y-center[1]);
+11            if(dis<=radius){
+12                if(q>bestQuality || (q==bestQuality && (x<bestx || x==bestx && y<besty))){
+13                    bestQuality=q;
+14                    bestx=x;
+15                    besty=y;
+16                }
+17            }
+18        }
+19        if(bestQuality==-1){
+20            return new int[] {-1,-1};
+21        }
+22        return new int[]{bestx,besty};
+23        
+24    }
+25}
+26
