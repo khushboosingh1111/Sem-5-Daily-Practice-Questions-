48ca1971e7ac5b42e999a47eccae0b528accf5ad
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-19 22:31:57 +0530

    File updated at 1/19/2026, 10:31:53 PM

--- 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
+++ 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
@@ -1,10 +1,10 @@
-// Last updated: 1/19/2026, 12:48:39 PM
+// Last updated: 1/19/2026, 10:31:53 PM
 1class Solution {
 2    public int sumSubarrayMins(int[] arr) {
 3        Stack<Integer> st=new Stack<>();
-4        int[] prevSmaller=new int[arr.length];
-5        int[] nextSmaller=new int[arr.length];
-6        int n=arr.length;
+4        int n=arr.length;
+5        int[] prevSmaller=new int[n];
+6        int[] nextSmaller=new int[n];
 7        Arrays.fill(prevSmaller,-1);
 8        Arrays.fill(nextSmaller,n);
 9        for(int i=0;i<n;i++){
@@ -22,10 +22,12 @@
 21        long sum=0;
 22        long mod = 1000000007;
 23        for(int i=0;i<n;i++){
-24            int right=nextSmaller[i]-i;
-25            int left=i-prevSmaller[i];
-26            sum=(sum + arr[i] * left % mod * right % mod) % mod;
-27        }
-28        return (int)sum;
-29    }
-30}
+24            int left=i-prevSmaller[i];
+25            int right=nextSmaller[i]-i;
+26            sum = (sum + arr[i] * left % mod * right % mod) % mod;
+27            
+28        }
+29        return (int)sum;
+30        
+31    }
+32}
