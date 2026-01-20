57064782e610e57f37573d422d24ecd1b98633f3
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-20 22:50:14 +0530

    File updated at 1/20/2026, 10:50:13 PM

--- 3634-MinimumRemovalstoBalanceArray/3634-MinimumRemovalstoBalanceArray.java
+++ 3634-MinimumRemovalstoBalanceArray/3634-MinimumRemovalstoBalanceArray.java
@@ -1,4 +1,4 @@
-// Last updated: 1/20/2026, 10:29:44 PM
+// Last updated: 1/20/2026, 10:50:13 PM
 1class Solution {
 2    public int minRemoval(int[] nums, int k) {
 3        if(nums.length == 1) return 0;
@@ -9,12 +9,14 @@
 8        int ans = 1;
 9
 10        for (int right = 0; right < n; right++) {
-11            while ((long) nums[right] > (long) nums[left] * k) {
-12                left++;
-13            }
-14            ans = Math.max(ans, right - left + 1);
-15        }
-16
-17        return n - ans;
-18    }
-19}
+11            //valid elements ki length tk
+12            while ((long) nums[right] > (long) nums[left] * k) {
+13                left++;
+14            }
+15            //kitne maximum element valid hai
+16            ans = Math.max(ans, right - left + 1);
+17        }
+18        //kitne elements dlt kren hai(total elements-valid elements)
+19        return n - ans;
+20    }
+21}
