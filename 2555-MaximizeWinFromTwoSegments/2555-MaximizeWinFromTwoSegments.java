cda099715f5e4c6818faeddd1926481b534e5128
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-22 23:05:45 +0530

    File updated at 1/22/2026, 11:05:43 PM

--- 2555-MaximizeWinFromTwoSegments/2555-MaximizeWinFromTwoSegments.java
+++ 2555-MaximizeWinFromTwoSegments/2555-MaximizeWinFromTwoSegments.java
@@ -1,29 +1,18 @@
-// Last updated: 1/22/2026, 11:05:14 PM
-1class Solution {
-2    public int maximizeWin(int[] arr, int k) {
-3        int n = arr.length;
-4        int[] left = new int[n];
-5        int[] right = new int[n];
-6        left[0] = 1;
-7        for(int i = 1, j = 0; i<n; i++){
-8            while(arr[i]-arr[j] > k){
-9                j++;
-10            }
-11            left[i] = Math.max(left[i-1],(i-j+1));
-12        }
-13
-14        right[n-1] = 1;
-15        for(int i = n-2, j = n-1; i>=0; i--){
-16            while(arr[j]- arr[i] > k){
-17                j--;
-18            }
-19            right[i] = Math.max(right[i+1],(j-i+1));
-20        } 
-21
-22        int ans = 0;
-23        for(int i = 0; i<=n; i++){
-24            ans = Math.max(ans,((i == 0) ? 0 : left[i-1]) + ((i == n) ? 0 : right[i]));
-25        }
-26        return ans;
-27    }
-28}
+// Last updated: 1/22/2026, 11:05:43 PM
+class Solution {
+    public int maximizeWin(int[] prizePositions, int k) {
+        if (2*k+1 >= prizePositions[prizePositions.length-1] - prizePositions[0]) return prizePositions.length;
+        int[] dp = new int[prizePositions.length+1];
+        int res = 0;
+        int left = 0;
+
+        for (int right = 0; right < prizePositions.length; right++) {
+            while (prizePositions[right] - k > prizePositions[left]) {
+                left++;
+            }
+            dp[right+1] = Math.max(dp[right], right-left+1);
+            res = Math.max(res, dp[left] + right-left+1);
+        }
+        return res;
+    }
+}
