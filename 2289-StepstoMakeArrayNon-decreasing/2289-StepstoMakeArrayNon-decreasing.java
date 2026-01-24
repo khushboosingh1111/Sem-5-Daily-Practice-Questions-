e2ef8d1066bc302f634a6e3832ebc5e50cf005bd
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-24 22:52:49 +0530

    File updated at 1/24/2026, 10:52:47 PM

--- 2289-StepstoMakeArrayNon-decreasing/2289-StepstoMakeArrayNon-decreasing.java
+++ 2289-StepstoMakeArrayNon-decreasing/2289-StepstoMakeArrayNon-decreasing.java
@@ -1,22 +1,15 @@
-// Last updated: 1/24/2026, 10:52:38 PM
-1class Solution {
-2    public int totalSteps(int[] nums) {
-3        int n = nums.length;
-4        int ans = 0;
-5        int[] steps = new int[n];
-6        int[] stk = new int[n]; 
-7        int top = -1; 
-8
-9        for (int i = n - 1; i >= 0; i--) {
-10            int maxStep = 0;
-11            while (top >= 0 && nums[i] > nums[stk[top]]) {
-12                maxStep = Math.max(maxStep + 1, steps[stk[top]]);
-13                top--;
-14            }
-15            steps[i] = maxStep;
-16            ans = Math.max(ans, steps[i]);
-17            stk[++top] = i;
-18        }
-19        return ans;
-20    }
-21}
+// Last updated: 1/24/2026, 10:52:47 PM
+class Solution {
+    public int totalSteps(int[] nums) {
+        int n = nums.length, res = 0, j = -1;
+        int dp[] = new int[n], stack[] = new int[n];
+        for(int i = n-1; i >= 0; --i){
+            while(j >= 0 && nums[i] > nums[stack[j]]){
+                dp[i] = Math.max(++dp[i], dp[stack[j--]]);
+                res = Math.max(dp[i], res);
+            }
+            stack[++j] = i;
+        }
+        return res;
+    }
+}
