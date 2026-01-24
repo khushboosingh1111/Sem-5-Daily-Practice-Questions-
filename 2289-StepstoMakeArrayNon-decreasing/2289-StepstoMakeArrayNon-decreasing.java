83dae87bf5c7ba66ff70ef2bbb422152bc3781ab
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-24 22:50:42 +0530

    File updated at 1/24/2026, 10:50:40 PM

--- 2289-StepstoMakeArrayNon-decreasing/2289-StepstoMakeArrayNon-decreasing.java
+++ 2289-StepstoMakeArrayNon-decreasing/2289-StepstoMakeArrayNon-decreasing.java
@@ -1,15 +1,33 @@
-// Last updated: 1/21/2026, 10:14:28 PM
-class Solution {    
-    public int totalSteps(int[] A) {
-        int n = A.length, res = 0, j = -1;
-        int dp[] = new int[n], stack[] = new int[n];
-        for (int i = n - 1; i >= 0; --i) {
-            while (j >= 0 && A[i] > A[stack[j]]) {
-                dp[i] = Math.max(++dp[i], dp[stack[j--]]);
-                res = Math.max(res, dp[i]);
-            }
-            stack[++j] = i;
-        }
-        return res;
-    }
-}
+// Last updated: 1/24/2026, 10:50:40 PM
+1class Solution {
+2    public int totalSteps(int[] nums) {
+3        int n = nums.length;
+4        int ans = 0;
+5
+6        Stack<Integer> valStack = new Stack<>(); //value or element
+7        Stack<Integer> stepStack = new Stack<>(); //steps or count
+8        
+9
+10        valStack.push(nums[n - 1]); //element
+11        stepStack.push(0); //count
+12
+13        
+14        for (int i = n - 2; i >= 0; i--) {
+15
+16            int count = 0;
+17
+18            while (!valStack.isEmpty() && nums[i] > valStack.peek()) {
+19                count = Math.max(count + 1, stepStack.peek());
+20                valStack.pop();
+21                stepStack.pop();
+22            }
+23
+24            ans = Math.max(ans, count);
+25            valStack.push(nums[i]);
+26            stepStack.push(count);
+27        }
+28
+29        return ans;
+30    }
+31}
+32
