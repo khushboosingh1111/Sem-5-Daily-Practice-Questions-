c4b2dac28434e87f1cd9509fb4d041a9469fcfe5
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-01-30 08:55:28 +0530

    File updated at 1/30/2026, 8:55:25 AM

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,4 +1,4 @@
-// Last updated: 1/30/2026, 8:48:29 AM
+// Last updated: 1/30/2026, 8:55:25 AM
 1class Solution {
 2    public List<List<Integer>> subsets(int[] nums) {
 3        List<Integer> ll=new ArrayList<>();
@@ -8,13 +8,15 @@
 7        return ll2;
 8    }
 9    public static void solve(int[] nums,List<Integer> ll,List<List<Integer>> ll2,int n, int idx){
-10        if(idx==n){
+10        if(ll.size()<=n){
 11            ll2.add(new ArrayList<>(ll));
-12            return;
-13        }
-14        solve(nums,ll,ll2,n,idx+1);
-15        ll.add(nums[idx]);
-16        solve(nums,ll,ll2,n,idx+1);
-17        ll.remove(ll.size()-1);
-18    }
-19}
+12        }
+13        for(int i=idx;i<n;i++){
+14            ll.add(nums[i]);
+15            solve(nums,ll,ll2,n,i+1);
+16            ll.remove(ll.size()-1);
+17
+18        }
+19        
+20    }
+21}
