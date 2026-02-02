2758c42223e8fd1999a94a557234fcd123d97ce6
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-02 11:46:57 +0530

    File updated at 2/2/2026, 11:46:55 AM

--- 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
+++ 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
@@ -1,33 +1,31 @@
-// Last updated: 1/19/2026, 10:32:59 PM
+// Last updated: 2/2/2026, 11:46:55 AM
 1class Solution {
 2    public int sumSubarrayMins(int[] arr) {
 3        Stack<Integer> st=new Stack<>();
 4        int n=arr.length;
-5        int[] prevSmaller=new int[n];
-6        int[] nextSmaller=new int[n];
-7        Arrays.fill(prevSmaller,-1);
-8        Arrays.fill(nextSmaller,n);
+5        int[] prevsmaller =new int[n];
+6        int[] nextsmaller=new int[n];
+7        Arrays.fill(prevsmaller,-1);
+8        Arrays.fill(nextsmaller,n);
 9        for(int i=0;i<n;i++){
 10            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
-11                nextSmaller[st.pop()]=i;
+11                nextsmaller[st.pop()]=i;
 12            }
 13            st.push(i);
 14        }
 15        for(int i=n-1;i>=0;i--){
 16            while(!st.isEmpty() && arr[st.peek()]>arr[i]){
-17                prevSmaller[st.pop()]=i;
+17                prevsmaller[st.pop()]=i;
 18            }
 19            st.push(i);
 20        }
 21        long sum=0;
-22        long mod = 1000000007;
+22        long mod=1000000007;
 23        for(int i=0;i<n;i++){
-24            int left=i-prevSmaller[i];
-25            int right=nextSmaller[i]-i;
+24            int left=i-prevsmaller[i];
+25            int right=nextsmaller[i]-i;
 26            sum=(sum+left%mod*right%mod*arr[i])%mod;
-27            
-28        }
-29        return (int)sum;
-30        
-31    }
-32}
+27        }
+28        return (int)sum;
+29    }
+30}
