7a83751ae139ab0e7094eeb46829eee28eee6c4e
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-03 12:12:48 +0530

    File updated at 2/3/2026, 12:12:43 PM

--- 1090-LargestValuesFromLabels/1090-LargestValuesFromLabels.java
+++ 1090-LargestValuesFromLabels/1090-LargestValuesFromLabels.java
@@ -1,4 +1,4 @@
-// Last updated: 2/3/2026, 12:03:16 PM
+// Last updated: 2/3/2026, 12:12:43 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -11,27 +11,25 @@
 10 */
 11class Solution {
 12    public int[] nextLargerNodes(ListNode head) {
-13        List<Integer> ll=new ArrayList<>();
-14        Stack<Integer> st=new Stack<>();
-15       
-16        while(head!=null){
-17            ll.add(head.val);
-18            head=head.next;
-19        } 
-20        int[] ans=new int[ll.size()];
-21        for(int i=ll.size()-1;i>=0;i--){
-22            while(!st.isEmpty() && st.peek()<=ll.get(i)){
-23                st.pop();
-24            }
-25            if(st.isEmpty()){
-26                ans[i]=0;
-27            }
-28            else{
-29                ans[i]=st.peek();
-30            }
-31            st.push(ll.get(i));
-32        }
-33        return ans;
-34        
-35    }
-36}
+13        Stack<Integer> st=new Stack<>();
+14        List<Integer> ll=new ArrayList<>();
+15        while(head!=null){
+16            ll.add(head.val);
+17            head=head.next;
+18        }
+19        int[] ans=new int[ll.size()];
+20        for(int i=ll.size()-1;i>=0;i--){
+21            while(!st.isEmpty() && st.peek()<=ll.get(i)){
+22                st.pop();
+23            }
+24            if(st.isEmpty()){
+25                ans[i]=0;
+26            }
+27            else{
+28                ans[i]=st.peek();
+29            }
+30            st.push(ll.get(i));
+31        }
+32        return ans;
+33    }
+34}
