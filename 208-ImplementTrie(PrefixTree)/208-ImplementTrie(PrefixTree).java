6fe9c19a109f92c5a029cb4d496e3cb2942a4586
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-04 12:34:30 +0530

    File updated at 2/4/2026, 12:34:29 PM

--- 208-ImplementTrie(PrefixTree)/208-ImplementTrie(PrefixTree).java
+++ 208-ImplementTrie(PrefixTree)/208-ImplementTrie(PrefixTree).java
@@ -1,68 +1,67 @@
-// Last updated: 2/4/2026, 12:31:26 PM
+// Last updated: 2/4/2026, 12:34:29 PM
 1class Trie {
 2    class Node{
-3        char ch;
-4        boolean isterminal;
-5        HashMap<Character,Node> child;
-6        public Node(char ch){
-7            this.ch = ch;
-8            child = new HashMap<>();
-9        }
-10    }
-11    private Node root;
-12    public Trie() {
-13        root = new Node('*');
+3		char ch;
+4		boolean isTerminal;
+5		HashMap<Character,Node> child;
+6		public Node(char ch) {
+7			this.ch=ch;
+8			child=new HashMap<>();
+9		}
+10	}
+11	private Node root;
+12	public Trie() {
+13        root=new Node('*'); //new node hoine pr node ka ek object bnega aur uske pass ch hoga aur boolean isterminal hoga jo initially false hoga,aur hashset hoga 0 size ka
 14    }
-15    
-16    public void insert(String word) {
-17        Node curr = root;
-18        for (int i = 0; i < word.length(); i++){
-19            char ch = word.charAt(i);
-20            if (curr.child.containsKey(ch)){
-21                curr = curr.child.get(ch);
-22            }
-23            else{
-24                Node nn = new Node(ch);
-25                curr.child.put(ch,nn);
-26                curr = nn;
-27            }
-28        }
-29        curr.isterminal = true;
+15	public void insert(String word) {
+16        Node curr=root;
+17        for(int i=0;i<word.length();i++) {
+18        	char ch=word.charAt(i);
+19        	if(curr.child.containsKey(ch)) {
+20        		curr=curr.child.get(ch);
+21        	}
+22        	else {
+23        		Node nn=new Node(ch);
+24        		curr.child.put(ch, nn);
+25        		curr=nn;
+26        	}
+27        }
+28        curr.isTerminal = true;
+29
 30    }
-31    
-32    public boolean search(String word) {
-33        Node curr = root;
-34        for (int i = 0; i < word.length(); i++){
-35            char ch = word.charAt(i);
-36            if (curr.child.containsKey(ch)){
-37                curr = curr.child.get(ch);
-38            }
-39            else{
-40                return false;
-41            }
-42        }
-43        return curr.isterminal;
-44    }
-45    
-46    public boolean startsWith(String word) {
-47        Node curr = root;
-48        for (int i = 0; i < word.length(); i++){
-49            char ch = word.charAt(i);
-50            if (curr.child.containsKey(ch)){
-51                curr = curr.child.get(ch);
-52            }
-53            else{
-54                return false;
-55            }
-56        }
-57        return true;
-58    }
-59}
-60
-61/**
-62 * Your Trie object will be instantiated and called as such:
-63 * Trie obj = new Trie();
-64 * obj.insert(word);
-65 * boolean param_2 = obj.search(word);
-66 * boolean param_3 = obj.startsWith(prefix);
-67 */
+31	public boolean search(String word) {
+32		Node curr=root;
+33        for(int i=0;i<word.length();i++) {
+34        	char ch=word.charAt(i);
+35        	if(curr.child.containsKey(ch)) {
+36        		curr=curr.child.get(ch);
+37        	}
+38        	else {
+39        		return false;
+40        	}
+41        }
+42        return curr.isTerminal;
+43    }
+44	public boolean startsWith(String prefix) {
+45		Node curr=root;
+46        for(int i=0;i<prefix.length();i++) {
+47        	char ch=prefix.charAt(i);
+48        	if(curr.child.containsKey(ch)) {
+49        		curr=curr.child.get(ch);
+50        	}
+51        	else {
+52        		return false;
+53        	}
+54        }
+55        return true;
+56    }
+57}
+58
+59
+60/**
+61 * Your Trie object will be instantiated and called as such:
+62 * Trie obj = new Trie();
+63 * obj.insert(word);
+64 * boolean param_2 = obj.search(word);
+65 * boolean param_3 = obj.startsWith(prefix);
+66 */
