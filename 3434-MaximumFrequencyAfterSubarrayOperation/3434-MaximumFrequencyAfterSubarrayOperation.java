3261728864c0b294051fc071529e106dc0dbe336
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-07 23:31:44 +0530

    File updated at 2/7/2026, 11:31:44 PM

--- 3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
+++ 3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
@@ -1,21 +1,19 @@
-// Last updated: 2/6/2026, 12:47:41 PM
-1public class Solution {
+// Last updated: 2/7/2026, 11:31:44 PM
+1class Solution {
 2    public int maxFrequency(int[] nums, int k) {
-3        int orig = 0;
-4        for (int num : nums) {
-5            if (num == k) orig++;
-6        }
-7        int maxGain = 0;
-8        for (int m = 1; m <= 50; m++) {
-9            if (m == k) continue;
-10            int current = 0, maxCurrent = 0;
-11            for (int num : nums) {
-12                current += (num == m) ? 1 : (num == k) ? -1 : 0;
-13                current = Math.max(current, 0);
-14                maxCurrent = Math.max(maxCurrent, current);
-15            }
-16            maxGain = Math.max(maxGain, maxCurrent);
-17        }
-18        return orig + maxGain;
-19    }
-20}
+3        HashMap<Integer,Integer> map = new HashMap<>();
+4        int countk = 0;
+5        int max = 0;
+6        for(int x : nums){
+7            if(x==k){
+8                countk++;
+9            }else{
+10                int curr = map.getOrDefault(x,0)-countk;
+11                curr = Math.max(1,curr+1);
+12                map.put(x,curr+countk);
+13                max = Math.max(max,curr);
+14            }
+15        }
+16        return countk+max;
+17    }
+18}
