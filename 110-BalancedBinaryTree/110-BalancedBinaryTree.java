df03357102e890aaa1296b6e2b174cea87015ada
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-08 21:52:29 +0530

    File updated at 2/8/2026, 9:52:25 PM

--- 110-BalancedBinaryTree/110-BalancedBinaryTree.java
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.java
@@ -1,32 +1,32 @@
-// Last updated: 8/6/2025, 10:16:56 AM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    private boolean result = true;
-    public boolean isBalanced(TreeNode root) {
-    maxDepth(root);
-    return result;
-}
-public int maxDepth(TreeNode root) {
-    if (root == null)
-        return 0;
-    int l = maxDepth(root.left);
-    int r = maxDepth(root.right);
-    if (Math.abs(l - r) > 1)
-        result = false;
-    return 1 + Math.max(l, r);
-    }
-}
+// Last updated: 2/8/2026, 9:52:25 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    private boolean result = true;
+18    public boolean isBalanced(TreeNode root) {
+19    maxDepth(root);
+20    return result;
+21}
+22public int maxDepth(TreeNode root) {
+23    if (root == null)
+24        return 0;
+25    int l = maxDepth(root.left);
+26    int r = maxDepth(root.right);
+27    if (Math.abs(l - r) > 1)
+28        result = false;
+29    return 1 + Math.max(l, r);
+30    }
+31}
