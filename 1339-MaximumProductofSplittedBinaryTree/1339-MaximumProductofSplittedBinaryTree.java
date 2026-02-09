86396ef14b8eb292f0b1e16c9547f1843a2c7524
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-09 23:24:23 +0530

    File updated at 2/9/2026, 11:24:22 PM

--- 1339-MaximumProductofSplittedBinaryTree/1339-MaximumProductofSplittedBinaryTree.java
+++ 1339-MaximumProductofSplittedBinaryTree/1339-MaximumProductofSplittedBinaryTree.java
@@ -1,54 +1,36 @@
-// Last updated: 2/9/2026, 12:04:29 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
- /**
- 
-    Solution 1: 
-        Traverse the tree using DFS, as we come up the level, we calculate left subtree sum and right subtree sum. 
-        Calculate the max between  ( (left + curr) * (right) ) vs ((right+curr) * (left))
-        return the modulo
-  */
-class Solution {
-    long maxValue = 0;
-    long totalSum = 0;
-    public int maxProduct(TreeNode root) {
-        totalSum = totalSum(root);
-        traverse(root, totalSum);
-
-        return (int)(maxValue % 1000000007);
-    }
-
-    public long totalSum(TreeNode root){
-        if(root == null){
-            return 0;
-        }
-
-        return totalSum(root.left) + root.val + totalSum(root.right);
-    }
-
-    public long traverse(TreeNode root, long total){
-        if(root == null){
-            return 0;
-        }
-
-        long subTotal = traverse(root.left, total) + traverse(root.right, total) + root.val;
-
-        long prod = (long)subTotal * (total - subTotal);
-        this.maxValue = Math.max(maxValue, prod);
-        return subTotal;
-    }
-}
+// Last updated: 2/9/2026, 11:24:22 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    static long total;
+18    static long maxAns;
+19    public int maxProduct(TreeNode root) {
+20        maxAns = 0;
+21        total = findSum(root);
+22        solve(root);
+23        return (int)(maxAns%1000000007);
+24    }
+25    public long findSum(TreeNode root){
+26        if(root==null)return 0;
+27        return (long)root.val+findSum(root.left)+findSum(root.right);
+28    }
+29    public long solve(TreeNode root){
+30        if(root==null) return 0;
+31        long curr = root.val+solve(root.left)+solve(root.right);
+32        maxAns = Math.max(maxAns, curr*(total-curr));
+33        return curr;
+34}
+35}
