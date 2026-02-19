ec1fb77ef622325c49ffa64c48467557cd16c7aa
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-19 22:50:55 +0530

    File updated at 2/19/2026, 10:50:53 PM

--- 696-CountBinarySubstrings/696-CountBinarySubstrings.java
+++ 696-CountBinarySubstrings/696-CountBinarySubstrings.java
@@ -1,21 +1,16 @@
-// Last updated: 2/19/2026, 10:37:10 PM
+// Last updated: 2/19/2026, 10:50:53 PM
 1class Solution {
 2    public int countBinarySubstrings(String s) {
-3        int[] groups = new int[s.length()];
-4        int t = 0;
-5        groups[0] = 1;
-6        for (int i = 1; i < s.length(); i++) {
-7            if (s.charAt(i-1) != s.charAt(i)) {
-8                groups[++t] = 1;
+3        int ans = 0, prev = 0, cur = 1;
+4        for (int i = 1; i < s.length(); i++) {
+5            if (s.charAt(i-1) != s.charAt(i)) {
+6                ans += Math.min(prev, cur);
+7                prev = cur;
+8                cur = 1;
 9            } else {
-10                groups[t]++;
+10                cur++;
 11            }
 12        }
-13
-14        int ans = 0;
-15        for (int i = 1; i <= t; i++) {
-16            ans += Math.min(groups[i-1], groups[i]);
-17        }
-18        return ans;
-19    }
-20}
+13        return ans + Math.min(prev, cur);
+14    }
+15}
