e42f0bd77c973cce2ac2c1e90e072688798121df
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-24 00:46:14 +0530

    Create 1461-CheckIfaStringContainsAllBinaryCodesofSizeK

---
+++ 1461-CheckIfaStringContainsAllBinaryCodesofSizeK/1461-CheckIfaStringContainsAllBinaryCodesofSizeK.java
@@ -0,0 +1,19 @@
+// Last updated: 2/24/2026, 12:46:13 AM
+1class Solution {
+2    public boolean hasAllCodes(String s, int k) {
+3        HashSet<String> set=new HashSet<>();
+4        int n=s.length();
+5        int c=1<<k; //pow(2,k)
+6        for(int i=k;i<=n;i++){
+7            String sub=s.substring(i-k,i);
+8            if(!set.contains(sub)){
+9                set.add(sub);
+10                c--;
+11            }
+12            if(c==0){
+13                return true;
+14            }
+15        }
+16        return false;
+17    }
+18}
