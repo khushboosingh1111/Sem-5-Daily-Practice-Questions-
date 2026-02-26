7827183511b4c80fa41f46d8714818eca98bf871
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-26 12:10:20 +0530

    File updated at 2/26/2026, 12:10:12 PM

--- 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.java
+++ 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.java
@@ -1,20 +1,18 @@
-// Last updated: 2/26/2026, 12:09:16 PM
-1import java.math.BigInteger;
-2class Solution {
-3    public int numSteps(String s) {
-4        BigInteger num = new BigInteger(s, 2);
-5        int c = 0;
-6        BigInteger one = BigInteger.ONE;
-7        BigInteger two = new BigInteger("2");
-8        
-9        while(!num.equals(one)){
-10            if(num.mod(two).equals(BigInteger.ZERO)){
-11                num = num.divide(two);
-12            } else {
-13                num = num.add(one);
-14            }
-15            c++;
-16        }
-17        return c;
-18    }
-19}
+// Last updated: 2/26/2026, 12:10:12 PM
+class Solution {
+    public int numSteps(String s) {
+        int n = s.length();
+        int carry = 0;
+        int res = 0;
+        for (int i = n - 1; i > 0; i--){
+            if (s.charAt(i) - '0' + carry == 1){
+                res += 2;
+                carry = 1;
+            }
+            else{
+                res++;
+            }
+        }
+        return res + carry;
+    }
+}
