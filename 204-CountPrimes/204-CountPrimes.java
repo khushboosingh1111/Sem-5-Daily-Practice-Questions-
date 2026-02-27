8f9923304b12094590e8508adb6abf83fba9b01e
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-27 22:05:38 +0530

    File updated at 2/27/2026, 10:05:37 PM

--- 204-CountPrimes/204-CountPrimes.java
+++ 204-CountPrimes/204-CountPrimes.java
@@ -1,30 +1,30 @@
-// Last updated: 8/6/2025, 10:15:47 AM
-class Solution {
-    public int countPrimes(int n) {
-        if(n<=2){
-            return 0;
-        }
-        return PrimeSieve(n);
-        
-    }
-    public static int PrimeSieve(int n) {
-		int [] ans=new int[n];
-		ans[0]=ans[1]=1;
-		for(int i=2;i*i<=ans.length;i++) {
-			if(ans[i]==0) { //i prime hai uske multiple ki jgh 1 rkh denge
-				for(int j=2;i*j<ans.length;j++) {
-					ans[i*j]=1;
-				}
-				
-			}
-		}
-		int c=0;
-		for(int i=2;i<ans.length;i++) {
-			if(ans[i]==0) {
-				c++;
-			}
-		}
-		return c;
-	}
-
-}
+// Last updated: 2/27/2026, 10:05:37 PM
+1class Solution {
+2    public int countPrimes(int n) {
+3        if(n<=2){
+4            return 0;
+5        }
+6        return PrimeSieve(n);
+7        
+8    }
+9    public static int PrimeSieve(int n) {
+10		int [] ans=new int[n];
+11		ans[0]=ans[1]=1;
+12		for(int i=2;i*i<=ans.length;i++) {
+13			if(ans[i]==0) { //i prime hai uske multiple ki jgh 1 rkh denge
+14				for(int j=2;i*j<ans.length;j++) {
+15					ans[i*j]=1;
+16				}
+17				
+18			}
+19		}
+20		int c=0;
+21		for(int i=2;i<ans.length;i++) {
+22			if(ans[i]==0) {
+23				c++;
+24			}
+25		}
+26		return c;
+27	}
+28
+29}
