5b10966af511d59bcece3f4e5503027f7558d3f2
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-28 22:48:51 +0530

    File updated at 2/28/2026, 10:48:49 PM

--- 338-CountingBits/338-CountingBits.java
+++ 338-CountingBits/338-CountingBits.java
@@ -1,15 +1,15 @@
-// Last updated: 2/28/2026, 10:23:09 PM
+// Last updated: 2/28/2026, 10:48:49 PM
 1class Solution {
 2    public int[] countBits(int n) {
 3        int[] ans=new int[n+1];
 4        for(int i=0;i<=n;i++){
 5            int c=0;
-6            for(int j=0;j<31;j++){
-7                if((i & (1 << j)) != 0){
-8                    c++;
-9                }
-10                ans[i]=c;
-11            }
+6            int num=i;
+7            while(num>0){
+8                c++;
+9                num=num&(num-1);
+10            }
+11            ans[i]=c;
 12        }
 13        return ans;
 14    }
