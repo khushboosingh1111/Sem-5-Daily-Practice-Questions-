abbb19d4ed930b54764657b4728d55b9e6ce7aeb
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-28 22:09:56 +0530

    File updated at 2/28/2026, 10:09:53 PM

--- 204-CountPrimes/204-CountPrimes.java
+++ 204-CountPrimes/204-CountPrimes.java
@@ -1,23 +1,27 @@
-// Last updated: 2/28/2026, 8:25:57 PM
+// Last updated: 2/28/2026, 10:09:53 PM
 1class Solution {
 2    public int countPrimes(int n) {
 3        if (n == 0) return 0;
-4        boolean[] isPrime = new boolean[n];
-5        Arrays.fill(isPrime, true);
-6        int count = 0;
-7        isPrime[0] = false;
-8        if (n > 1) isPrime[1] = false;
-9        for (int i = 0; i < n; i++) {
-10            if (isPrime[i]) {
-11                count++;
-12                int j = i * 2;
-13                while (j < n) {
-14                    isPrime[j] = false;
-15                    j = j + i;
-16                }
-17            }
-18        }
-19        
-20        return count;
-21    }
-22}
+4        if(n==2) return 0;
+5        if(n==1) return 0;
+6        boolean[] isPrime = new boolean[n];
+7        Arrays.fill(isPrime, true);
+8        int count = n/2;
+9        isPrime[0] = false;
+10        if (n > 1) isPrime[1] = false;
+11        for (int i = 3; i*i < n; i+=2) {
+12            if (isPrime[i]) {
+13                int j = i * i;
+14                while (j < n) {
+15                    if(isPrime[j]){
+16                        count--;
+17                    }
+18                    isPrime[j] = false;
+19                    j+= i*2;
+20                }
+21            }
+22        }
+23        
+24        return count;
+25    }
+26}
