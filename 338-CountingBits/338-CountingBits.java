20f7a63f0bc0cfcd484d332bd6503bd79c327c42
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-02-28 22:23:11 +0530

    File updated at 2/28/2026, 10:23:09 PM

--- 338-CountingBits/338-CountingBits.java
+++ 338-CountingBits/338-CountingBits.java
@@ -1,31 +1,16 @@
-// Last updated: 10/12/2025, 5:21:41 PM
-class Solution {
-    public int[] countBits(int n) {
-        //top down
-        // int[] dp=new int[n+1];
-        // for(int i=1;i<=n;i++){
-        //     dp[i]=dp[i/2]+(i%2);
-        // }
-        // return dp;
-
-        //calling bootom up-
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0]=0;
-        for(int i=0;i<=n;i++){
-            cb(i,dp);
-        }
-        return dp;
-    }
-    //bottom up
-    public int cb(int i,int[] dp){
-        if(i==0){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        dp[i]=cb(i/2,dp)+(i%2);
-        return dp[i];
-    }
-}
+// Last updated: 2/28/2026, 10:23:09 PM
+1class Solution {
+2    public int[] countBits(int n) {
+3        int[] ans=new int[n+1];
+4        for(int i=0;i<=n;i++){
+5            int c=0;
+6            for(int j=0;j<31;j++){
+7                if((i & (1 << j)) != 0){
+8                    c++;
+9                }
+10                ans[i]=c;
+11            }
+12        }
+13        return ans;
+14    }
+15}
