57f2b3fa66dc5f1d32c0ff2d231ef19e5912315b
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-05 21:04:18 +0530

    File updated at 3/5/2026, 9:04:14 PM

--- 1758-MinimumChangesToMakeAlternatingBinaryString/1758-MinimumChangesToMakeAlternatingBinaryString.java
+++ 1758-MinimumChangesToMakeAlternatingBinaryString/1758-MinimumChangesToMakeAlternatingBinaryString.java
@@ -1,25 +1,20 @@
-// Last updated: 3/5/2026, 9:01:21 PM
+// Last updated: 3/5/2026, 9:04:14 PM
 1class Solution {
 2    public int minOperations(String s) {
 3        int start0 = 0;
-4        int start1 = 0;
-5        
-6        for (int i = 0; i < s.length(); i++) {
-7            if (i % 2 == 0) {
-8                if (s.charAt(i) == '0') {
-9                    start1++;
-10                } else {
-11                    start0++;
-12                }
-13            } else {
-14                if (s.charAt(i) == '1') {
-15                    start1++;
-16                } else {
-17                    start0++;
-18                }
-19            }
-20        }
-21        
-22        return Math.min(start0, start1);
-23    }
-24}
+4        
+5        for (int i = 0; i < s.length(); i++) {
+6            if (i % 2 == 0) {
+7                if (s.charAt(i) == '1') {
+8                    start0++;
+9                }
+10            } else {
+11                if (s.charAt(i) == '0') {
+12                    start0++;
+13                }
+14            }
+15        }
+16        
+17        return Math.min(start0, s.length() - start0);
+18    }
+19}
