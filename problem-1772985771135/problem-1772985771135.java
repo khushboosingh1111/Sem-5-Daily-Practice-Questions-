019dcb0497a405d4108bee99a22444712c274301
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-08 21:32:55 +0530

    Create

---
+++ problem-1772985771135/problem-1772985771135.java
@@ -0,0 +1,12 @@
+// Last updated: 3/8/2026, 9:32:51 PM
+1class Solution {
+2    public String findDifferentBinaryString(String[] nums) {
+3        StringBuilder ans = new StringBuilder();
+4        for (int i = 0; i < nums.length; i++) {
+5            Character curr = nums[i].charAt(i);
+6            ans.append(curr == '0' ? '1' : '0');
+7        }
+8        
+9        return ans.toString();
+10    }
+11}
