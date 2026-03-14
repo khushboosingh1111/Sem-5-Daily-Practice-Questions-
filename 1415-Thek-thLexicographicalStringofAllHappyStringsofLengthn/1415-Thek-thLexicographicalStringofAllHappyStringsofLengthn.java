e4594b65f06a2d3af7be8234f56ddc3c5eac940a
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-14 23:21:22 +0530

    File updated at 3/14/2026, 11:21:22 PM

--- 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn/1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn.java
+++ 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn/1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn.java
@@ -1,41 +1,37 @@
-// Last updated: 3/14/2026, 11:20:23 PM
-class Solution {
-
-    private static char[] ALLOWED_LETTERS = { 'a', 'b', 'c' };
-
-    private int happyStringCount;
-    private String kthHappyString;
-
-    public String getHappyString(int n, int k) {
-        this.happyStringCount = 0;
-        this.kthHappyString = "";
-        kthHappyStringHelper(new StringBuilder(), n, k);
-        return happyStringCount >= k ? kthHappyString : "";
-    }
-
-    private void kthHappyStringHelper(StringBuilder currStr, int n, int k) {
-
-        int currStrSize = currStr.length();
-
-        if (currStrSize == n) {
-            happyStringCount++;
-            if (happyStringCount == k) {
-                kthHappyString = currStr.toString();
-            }
-            return;
-        }
-
-        for (char c : ALLOWED_LETTERS) {
-
-            if (currStrSize > 0 && c == currStr.charAt(currStrSize - 1))
-                continue;
-
-            currStr.append(c);
-            kthHappyStringHelper(currStr, n, k);
-            currStr.deleteCharAt(currStrSize);
-
-            if (happyStringCount == k)
-                return;
-        }
-    }
-}
+// Last updated: 3/14/2026, 11:21:22 PM
+1class Solution {
+2    int count = 0;
+3    String result = "";
+4    public String getHappyString(int n, int k) {
+5        StringBuilder sb = new StringBuilder();
+6        dfs(sb, n, k);
+7
+8        return result;
+9    }
+10    private void dfs(StringBuilder sb, int n, int k) {
+11
+12        if (sb.length() == n) {
+13            count++;
+14
+15            if (count == k) {
+16                result = sb.toString();
+17            }
+18            return;
+19        }
+20
+21        char[] chars = {'a', 'b', 'c'};
+22
+23        for (char c : chars) {
+24
+25            // avoid same adjacent characters
+26            if (sb.length() > 0 && sb.charAt(sb.length() - 1) == c)
+27                continue;
+28
+29            sb.append(c);                 // choose
+30            dfs(sb, n, k);                // explore
+31            sb.deleteCharAt(sb.length() - 1); // backtrack
+32
+33            if (!result.equals("")) return; // stop early if found
+34        }
+35    }
+36}
