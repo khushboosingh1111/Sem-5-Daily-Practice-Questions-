68d359ec0f0191c0703eb07f5beed5e8878cf37d
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-14 23:20:23 +0530

    File updated at 3/14/2026, 11:20:23 PM

--- 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn/1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn.java
+++ 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn/1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn.java
@@ -1,37 +1,41 @@
-// Last updated: 3/14/2026, 11:20:15 PM
-1class Solution {
-2
-3    List<String> happyStrings = new ArrayList<>();
-4
-5    public String getHappyString(int n, int k) {
-6        String currentString = "";
-7        // Generate all happy strings of length n
-8        generateHappyStrings(n, currentString);
-9
-10        // Check if there are at least k happy strings
-11        if (happyStrings.size() < k) return "";
-12
-13        return happyStrings.get(k - 1);
-14    }
-15
-16    protected void generateHappyStrings(int n, String currentString) {
-17        // If the current string has reached the desired length, add it to the
-18        // list
-19        if (currentString.length() == n) {
-20            happyStrings.add(currentString);
-21            return;
-22        }
-23
-24        // Try adding each character ('a', 'b', 'c') to the current string
-25        for (char currentChar = 'a'; currentChar <= 'c'; currentChar++) {
-26            // Skip if the current character is the same as the last character
-27            if (
-28                currentString.length() > 0 &&
-29                currentString.charAt(currentString.length() - 1) == currentChar
-30            ) continue;
-31
-32            // Recursively generate the next character
-33            generateHappyStrings(n, currentString + currentChar);
-34        }
-35    }
-36}
+// Last updated: 3/14/2026, 11:20:23 PM
+class Solution {
+
+    private static char[] ALLOWED_LETTERS = { 'a', 'b', 'c' };
+
+    private int happyStringCount;
+    private String kthHappyString;
+
+    public String getHappyString(int n, int k) {
+        this.happyStringCount = 0;
+        this.kthHappyString = "";
+        kthHappyStringHelper(new StringBuilder(), n, k);
+        return happyStringCount >= k ? kthHappyString : "";
+    }
+
+    private void kthHappyStringHelper(StringBuilder currStr, int n, int k) {
+
+        int currStrSize = currStr.length();
+
+        if (currStrSize == n) {
+            happyStringCount++;
+            if (happyStringCount == k) {
+                kthHappyString = currStr.toString();
+            }
+            return;
+        }
+
+        for (char c : ALLOWED_LETTERS) {
+
+            if (currStrSize > 0 && c == currStr.charAt(currStrSize - 1))
+                continue;
+
+            currStr.append(c);
+            kthHappyStringHelper(currStr, n, k);
+            currStr.deleteCharAt(currStrSize);
+
+            if (happyStringCount == k)
+                return;
+        }
+    }
+}
