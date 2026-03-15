1bc3a290e7024a223f109afa5359e6d51f3886f9
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-15 20:42:33 +0530

    File updated at 3/15/2026, 8:42:30 PM

--- 3866-FirstUniqueEvenElement/3866-FirstUniqueEvenElement.java
+++ 3866-FirstUniqueEvenElement/3866-FirstUniqueEvenElement.java
@@ -1,22 +1,9 @@
-// Last updated: 3/15/2026, 8:41:56 PM
-1import java.util.*;
-2
-3class Solution {
-4    public int firstUniqueEven(int[] nums) {
-5        HashMap<Integer, Integer> map = new HashMap<>();
-6
-7        for(int num : nums){
-8            if(num % 2 == 0){
-9                map.put(num, map.getOrDefault(num, 0) + 1);
-10            }
-11        }
-12
-13        for(int num : nums){
-14            if(num % 2 == 0 && map.get(num) == 1){
-15                return num;
-16            }
-17        }
-18
-19        return -1;
-20    }
-21}
+// Last updated: 3/15/2026, 8:42:30 PM
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+        int[] freq = new int[101];
+        for (int x : nums) if (x % 2 == 0) freq[x]++;
+        for (int x : nums) if (x % 2 == 0 && freq[x] == 1) return x;
+        return -1;
+    }
+}
