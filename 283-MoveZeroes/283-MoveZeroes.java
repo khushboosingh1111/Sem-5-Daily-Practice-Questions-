83f5b1673907f3ed2861bc6c8ff2f88ec6f22580
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-17 12:42:30 +0530

    File updated at 3/17/2026, 12:42:25 PM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,15 +1,14 @@
-// Last updated: 8/6/2025, 10:15:11 AM
-class Solution {
-    public void moveZeroes(int[] nums) {
-        int left=0;
-        for(int right=0;right<nums.length;right++){
-            if(nums[right]!=0){
-                int temp=nums[right];
-                nums[right]=nums[left];
-                nums[left]=temp;
-                left++;
-            }
-        }
-        
-    }
-}
+// Last updated: 3/17/2026, 12:42:25 PM
+1class Solution {
+2    public void moveZeroes(int[] nums) {
+3        int left=0;
+4        for(int i=0;i<nums.length;i++){
+5            if(nums[i]!=0){
+6                int temp=nums[i];
+7                nums[i]=nums[left];
+8                nums[left]=temp;
+9                left++;
+10            }
+11        }
+12    }
+13}
