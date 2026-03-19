6b46fb5219f7f2bd53cf768f731d2507afe32def
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-19 19:27:10 +0530

    Create

---
+++ problem-1773928625436/problem-1773928625436.java
@@ -0,0 +1,18 @@
+// Last updated: 3/19/2026, 7:27:05 PM
+1class Solution {
+2    public void rotate(int[] nums, int k) {
+3        k=k%nums.length;
+4        reverse(nums,0,nums.length-1);
+5        reverse(nums,0,k-1);
+6        reverse(nums,k,nums.length-1);
+7    }
+8    public static void reverse(int[] nums,int l,int r){
+9        while(l<r){
+10            int temp=nums[l];
+11            nums[l]=nums[r];
+12            nums[r]=temp;
+13            l++;
+14            r--;
+15        }
+16    }
+17}
