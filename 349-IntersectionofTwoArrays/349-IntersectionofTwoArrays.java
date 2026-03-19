e1eb61d7334e1c732d2609a796ad33e99be73190
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-19 22:23:30 +0530

    File updated at 3/19/2026, 10:23:30 PM

--- 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
@@ -1,26 +1,42 @@
-// Last updated: 3/19/2026, 10:21:59 PM
-1import java.util.*;
-2
-3class Solution {
-4    public int[] intersection(int[] nums1, int[] nums2) {
-5        HashMap<Integer, Integer> map = new HashMap<>();
-6        // store nums1 elements
-7        for(int num : nums1){
-8            map.put(num, 1);
-9        }
-10        ArrayList<Integer> list = new ArrayList<>();
-11        for(int num : nums2){
-12            if(map.containsKey(num)){
-13                list.add(num);
-14                map.remove(num); // to avoid duplicates
-15            }
-16        }
-17        // convert list to array
-18        int[] ans = new int[list.size()];
-19        for(int i = 0; i < list.size(); i++){
-20            ans[i] = list.get(i);
-21        }
-22
-23        return ans;
-24    }
-25}
+// Last updated: 3/19/2026, 10:23:30 PM
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        boolean[] present = new boolean[1001];
+
+         for(int i:nums1 ){
+            present[i] = true;
+        }
+
+        int[] res=new int[Math.max(nums1.length,nums2.length)];
+        int k=0;
+
+        for(int i:nums2){
+            if(present[i]){
+                res[k++]=i;
+                present[i] = false;
+            }
+        }
+        return Arrays.copyOf(res,k);
+
+        // HashSet<Integer> set = new HashSet<>();
+        // HashSet<Integer> res = new HashSet<>();
+
+        // for(int i:nums2 ){
+        //     set.add(i);
+        // }
+
+        // for(int i : nums1){
+        //     if(set.contains(i)){
+        //         res.add(i);
+        //     }
+        // } 
+        // int[] ans = new int[res.size()];
+        // int index=0;
+        // for(int i:res){
+        //     ans[index++] = i;
+        // }
+        
+        // return ans;
+    }
+}
