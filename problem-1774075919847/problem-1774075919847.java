ea3953235116ecc00ffeebf75e20322daff594a3
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-21 12:22:02 +0530

    Create

---
+++ problem-1774075919847/problem-1774075919847.java
@@ -0,0 +1,22 @@
+// Last updated: 3/21/2026, 12:21:59 PM
+class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+
+        int top = x;
+        int bottom = x+k-1;
+        while(top < bottom){
+
+
+            for(int j=y;j<y+k;j++){
+
+
+                int temp = grid[top][j];
+                grid[top][j] = grid[bottom][j];
+                grid[bottom][j] = temp;
+            }
+            top++;
+            bottom--;
+        }
+        return grid;
+    }
+}
