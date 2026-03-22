37e70ea98288baed65f7b09d552bd511c736a252
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-22 20:06:06 +0530

    File updated at 3/22/2026, 8:06:00 PM

--- 1886-DetermineWhetherMatrixCanBeObtainedByRotation/1886-DetermineWhetherMatrixCanBeObtainedByRotation.java
+++ 1886-DetermineWhetherMatrixCanBeObtainedByRotation/1886-DetermineWhetherMatrixCanBeObtainedByRotation.java
@@ -1,37 +1,53 @@
-// Last updated: 3/22/2026, 8:04:58 PM
-1class Solution {
-2
-3    public boolean findRotation(int[][] mat, int[][] target) {
-4        int n = mat.length;
-5        // at most 4 rotations
-6        for (int k = 0; k < 4; k++) {
-7            // rotation operation
-8            for (int i = 0; i < n / 2; i++) {
-9                for (int j = 0; j < (n + 1) / 2; j++) {
-10                    int temp = mat[i][j];
-11                    mat[i][j] = mat[n - 1 - j][i];
-12                    mat[n - 1 - j][i] = mat[n - 1 - i][n - 1 - j];
-13                    mat[n - 1 - i][n - 1 - j] = mat[j][n - 1 - i];
-14                    mat[j][n - 1 - i] = temp;
-15                }
-16            }
-17
-18            if (isEqual(mat, target)) {
-19                return true;
-20            }
-21        }
-22        return false;
-23    }
-24
-25    private boolean isEqual(int[][] mat, int[][] target) {
-26        int n = mat.length;
-27        for (int i = 0; i < n; i++) {
-28            for (int j = 0; j < n; j++) {
-29                if (mat[i][j] != target[i][j]) {
-30                    return false;
-31                }
-32            }
-33        }
-34        return true;
-35    }
-36}
+// Last updated: 3/22/2026, 8:06:00 PM
+class Solution {
+    public void swap(int [][] arr,int i,int j){
+        int temp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = temp;
+    }
+
+    public void reverse(int [] arr){
+        int n = arr.length;
+        for(int i = 0;i < n/2;i++){
+            int temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-1-i] = temp;  
+        }
+    }
+
+    public void rotate(int [][] mat){
+        int n = mat.length;
+        for(int i2 = 0;i2 < n-1;i2++){
+            for(int j = i2+1;j < n;j++){
+                swap(mat,i2,j);
+            }
+        }
+        for(int i1 = 0;i1 < n;i1++){
+            reverse(mat[i1]);
+        }
+
+    }
+
+    public boolean isEqual(int[][] mat, int[][] target){
+        int n = mat.length;
+        for(int i = 0;i < n;i++){
+        for(int j = 0;j < n;j++){
+            if(mat[i][j] != target[i][j]){
+                return false;
+            }
+        }
+    }
+    return true;
+    }
+
+    public boolean findRotation(int[][] mat, int[][] target) {
+        int n = mat.length;
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n;j++){
+                if(isEqual(mat,target)) return true;
+                rotate(mat);
+            }
+        }
+    return false;
+}
+}
