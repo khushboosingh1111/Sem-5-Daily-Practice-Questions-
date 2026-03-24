0e7b6097325b57b79f950cee2701513272a4f9b8
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-24 12:29:36 +0530

    File updated at 3/24/2026, 12:29:36 PM

--- 860-LemonadeChange/860-LemonadeChange.java
+++ 860-LemonadeChange/860-LemonadeChange.java
@@ -1,40 +1,33 @@
-// Last updated: 3/24/2026, 12:26:14 PM
-class Solution {
-    public boolean lemonadeChange(int[] bills) {
-        int five = 0;
-        int ten =0;
-        int twenty =0;
-        int i =0;
-        int n =bills.length;
-        while(i<n){
-            if(bills[i]==5){
-                five++;
-                i++;
-            }
-            else if(bills[i]==10){
-                if(five==0 || five==-1){
-                    return false;
-                }
-                else{
-                    ten++;
-                    five--;
-                }
-                i++;
-            }
-            else{
-                if(ten>0 && five>0){
-                    ten--;
-                    five--;
-                }
-                else if (five>=3){
-                    five -= 3;
-                }
-                else{
-                    return false;
-                }
-                i++;
-            }
-        }
-        return true;
-    }
-}
+// Last updated: 3/24/2026, 12:29:36 PM
+1class Solution {
+2    public boolean lemonadeChange(int[] bills) {
+3        int five=0;
+4        int ten=0;
+5        for(int i:bills){
+6            if(i==5){
+7                five++;
+8            }
+9            else if(i==10){
+10                if(five>0){
+11                    five--;
+12                    ten++;
+13                }
+14                else{
+15                    return false;
+16                }
+17            }
+18            else{
+19                if(five>0 && ten>0){
+20                    five--;
+21                    ten--;
+22                }else if(five>=3){
+23                    five-=3;
+24                }
+25                else{
+26                    return false;
+27                }
+28            }
+29        }   
+30        return true;
+31    }
+32}
