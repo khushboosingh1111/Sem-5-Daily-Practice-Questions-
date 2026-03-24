53e7a53e02d61ac6e87f59ab0b46577d8b41071b
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-24 12:26:14 +0530

    File updated at 3/24/2026, 12:26:14 PM

--- 860-LemonadeChange/860-LemonadeChange.java
+++ 860-LemonadeChange/860-LemonadeChange.java
@@ -1,41 +1,40 @@
-// Last updated: 3/24/2026, 12:25:53 PM
-1class Solution {
-2
-3    public boolean lemonadeChange(int[] bills) {
-4        // Count of $5 and $10 bills in hand
-5        int fiveDollarBills = 0;
-6        int tenDollarBills = 0;
-7
-8        // Iterate through each customer's bill
-9        for (int customerBill : bills) {
-10            if (customerBill == 5) {
-11                // Just add it to our count
-12                fiveDollarBills++;
-13            } else if (customerBill == 10) {
-14                // We need to give $5 change
-15                if (fiveDollarBills > 0) {
-16                    fiveDollarBills--;
-17                    tenDollarBills++;
-18                } else {
-19                    // Can't provide change, return false
-20                    return false;
-21                }
-22            } else { // customerBill == 20
-23                // We need to give $15 change
-24                if (tenDollarBills > 0 && fiveDollarBills > 0) {
-25                    // Give change as one $10 and one $5
-26                    fiveDollarBills--;
-27                    tenDollarBills--;
-28                } else if (fiveDollarBills >= 3) {
-29                    // Give change as three $5
-30                    fiveDollarBills -= 3;
-31                } else {
-32                    // Can't provide change, return false
-33                    return false;
-34                }
-35            }
-36        }
-37        // If we've made it through all customers, return true
-38        return true;
-39    }
-40}
+// Last updated: 3/24/2026, 12:26:14 PM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten =0;
+        int twenty =0;
+        int i =0;
+        int n =bills.length;
+        while(i<n){
+            if(bills[i]==5){
+                five++;
+                i++;
+            }
+            else if(bills[i]==10){
+                if(five==0 || five==-1){
+                    return false;
+                }
+                else{
+                    ten++;
+                    five--;
+                }
+                i++;
+            }
+            else{
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }
+                else if (five>=3){
+                    five -= 3;
+                }
+                else{
+                    return false;
+                }
+                i++;
+            }
+        }
+        return true;
+    }
+}
