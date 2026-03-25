8d1863d3742e0b19f6e9ad96dbd1f4f17bfc42ed
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-25 22:58:24 +0530

    Create 3546-EqualSumGridPartitionI

---
+++ 3546-EqualSumGridPartitionI/3546-EqualSumGridPartitionI.java
@@ -0,0 +1,25 @@
+// Last updated: 3/25/2026, 10:58:20 PM
+1class Solution {
+2    public boolean canPartitionGrid(int[][] grid) {
+3        int m = grid.length, n = grid[0].length;
+4        long total = 0;
+5        for (int[] row : grid)
+6            for (int x : row)
+7                total += x;
+8        if ((total & 1) == 1) return false;
+9        long target = total / 2, sum = 0;
+10        for (int i = 0; i < m - 1; i++) {
+11            for (int j = 0; j < n; j++)
+12                sum += grid[i][j];
+13            if (sum == target) return true;
+14        }
+15        sum = 0;
+16        for (int j = 0; j < n - 1; j++) {
+17            for (int i = 0; i < m; i++)
+18                sum += grid[i][j];
+19            if (sum == target) return true;
+20        }
+21        
+22        return false;
+23    }
+24}
