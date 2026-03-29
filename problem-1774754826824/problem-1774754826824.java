d3dcadc62a6e49fe075176e645fe8dcec7537311
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-03-29 08:57:16 +0530

    Create

---
+++ problem-1774754826824/problem-1774754826824.java
@@ -0,0 +1,9 @@
+// Last updated: 3/29/2026, 8:57:06 AM
+1class Solution {
+2    public boolean canBeEqual(String s1, String s2) {
+3        return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+4                (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0))) &&
+5               ((s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+6                (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1)));
+7    }
+8}
