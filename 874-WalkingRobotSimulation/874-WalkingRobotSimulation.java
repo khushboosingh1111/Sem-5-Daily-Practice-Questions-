30accb0de686821fbe6d01f3ae1cf2903d627481
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-07 14:38:38 +0530

    File updated at 4/7/2026, 2:38:38 PM

--- 874-WalkingRobotSimulation/874-WalkingRobotSimulation.java
+++ 874-WalkingRobotSimulation/874-WalkingRobotSimulation.java
@@ -1,4 +1,4 @@
-// Last updated: 4/7/2026, 2:37:51 PM
+// Last updated: 4/7/2026, 2:38:38 PM
 1class Solution {
 2    private static final long HASH_MULTIPLIER = 60013; // Slightly larger than 2 * max coordinate value
 3    public int robotSim(int[] commands, int[][] obstacles) {
@@ -23,30 +23,29 @@
 22                currentDirection = (currentDirection + 3) % 4;
 23                continue;
 24            }
-25
-26            // Move forward
-27            int[] direction = directions[currentDirection];
-28            for (int step = 0; step < command; step++) {
-29                int nextX = currentPosition[0] + direction[0];
-30                int nextY = currentPosition[1] + direction[1];
-31                if (obstacleSet.contains(hashCoordinates(nextX, nextY))) {
-32                    break;
-33                }
-34                currentPosition[0] = nextX;
-35                currentPosition[1] = nextY;
-36            }
-37
-38            maxDistanceSquared = Math.max(
-39                maxDistanceSquared,
-40                currentPosition[0] * currentPosition[0] +
-41                currentPosition[1] * currentPosition[1]
-42            );
-43        }
-44
-45        return maxDistanceSquared;
-46    }
-47    // Hash function to convert (x, y) coordinates to a unique integer value
-48    private long hashCoordinates(long x, long y) {
-49        return x + HASH_MULTIPLIER * y;
-50    }
-51}
+25            // Move forward
+26            int[] direction = directions[currentDirection];
+27            for (int step = 0; step < command; step++) {
+28                int nextX = currentPosition[0] + direction[0];
+29                int nextY = currentPosition[1] + direction[1];
+30                if (obstacleSet.contains(hashCoordinates(nextX, nextY))) {
+31                    break;
+32                }
+33                currentPosition[0] = nextX;
+34                currentPosition[1] = nextY;
+35            }
+36
+37            maxDistanceSquared = Math.max(
+38                maxDistanceSquared,
+39                currentPosition[0] * currentPosition[0] +
+40                currentPosition[1] * currentPosition[1]
+41            );
+42        }
+43
+44        return maxDistanceSquared;
+45    }
+46    // Hash function to convert (x, y) coordinates to a unique integer value
+47    private long hashCoordinates(long x, long y) {
+48        return x + HASH_MULTIPLIER * y;
+49    }
+50}
