9e0df4982c27cd1b171046726a0f2887d0058172
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-08 20:26:06 +0530

    File updated at 4/8/2026, 8:26:04 PM

--- 3653-XORAfterRangeMultiplicationQueriesI/3653-XORAfterRangeMultiplicationQueriesI.java
+++ 3653-XORAfterRangeMultiplicationQueriesI/3653-XORAfterRangeMultiplicationQueriesI.java
@@ -1,4 +1,4 @@
-// Last updated: 4/8/2026, 8:25:54 PM
+// Last updated: 4/8/2026, 8:26:04 PM
 1class Solution {
 2    private static final int MOD = 1_000_000_007;
 3    public int xorAfterQueries(int[] nums, int[][] queries) {
