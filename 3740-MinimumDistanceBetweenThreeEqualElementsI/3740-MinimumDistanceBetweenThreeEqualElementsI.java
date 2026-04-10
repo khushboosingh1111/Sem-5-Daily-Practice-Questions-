488f3ecb44c8d3248af6ed0ff01341514f0b188d
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-10 14:06:42 +0530

    File updated at 4/10/2026, 2:06:40 PM

--- 3740-MinimumDistanceBetweenThreeEqualElementsI/3740-MinimumDistanceBetweenThreeEqualElementsI.java
+++ 3740-MinimumDistanceBetweenThreeEqualElementsI/3740-MinimumDistanceBetweenThreeEqualElementsI.java
@@ -1,26 +1,26 @@
-// Last updated: 11/16/2025, 11:55:27 PM
-class Solution {
-    public int minimumDistance(int[] nums) {
-        Map<Integer,List<Integer>> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.putIfAbsent(nums[i],new ArrayList<>());
-            map.get(nums[i]).add(i);
-        }
-        int ans=Integer.MAX_VALUE;
-        for(int ke:map.keySet()){
-            List<Integer>l=map.get(ke);
-            Collections.sort(l);
-            if(l.size()>2){
-                for(int i=0;i<l.size()-2;i++){
-                    int a=l.get(i);
-                    int b=l.get(i+1);
-                    int c=l.get(i+2);
-                    int dif=Math.abs(a-b)+Math.abs(b-c)+Math.abs(c-a);
-                    ans=Math.min(ans,dif);
-                }
-            }
-        }
-        return ans==Integer.MAX_VALUE ?-1:ans;
-        
-    }
-}
+// Last updated: 4/10/2026, 2:06:40 PM
+1class Solution {
+2    public int minimumDistance(int[] nums) {
+3        Map<Integer,List<Integer>> map=new HashMap<>();
+4        for(int i=0;i<nums.length;i++){
+5            map.putIfAbsent(nums[i],new ArrayList<>());
+6            map.get(nums[i]).add(i);
+7        }
+8        int ans=Integer.MAX_VALUE;
+9        for(int ke:map.keySet()){
+10            List<Integer>l=map.get(ke);
+11            Collections.sort(l);
+12            if(l.size()>2){
+13                for(int i=0;i<l.size()-2;i++){
+14                    int a=l.get(i);
+15                    int b=l.get(i+1);
+16                    int c=l.get(i+2);
+17                    int dif=Math.abs(a-b)+Math.abs(b-c)+Math.abs(c-a);
+18                    ans=Math.min(ans,dif);
+19                }
+20            }
+21        }
+22        return ans==Integer.MAX_VALUE ?-1:ans;
+23        
+24    }
+25}
