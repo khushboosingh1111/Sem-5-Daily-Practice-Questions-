1580dca4ef5b8b7b5b42aa3995a073f9a0390614
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-11 12:43:59 +0530

    File updated at 4/11/2026, 12:43:54 PM

--- 3741-MinimumDistanceBetweenThreeEqualElementsII/3741-MinimumDistanceBetweenThreeEqualElementsII.java
+++ 3741-MinimumDistanceBetweenThreeEqualElementsII/3741-MinimumDistanceBetweenThreeEqualElementsII.java
@@ -1,27 +1,22 @@
-// Last updated: 4/11/2026, 12:43:19 PM
-class Solution {
-    public int minimumDistance(int[] nums) {
-        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            ArrayList<Integer> l=map.getOrDefault(nums[i],new ArrayList<>());
-            l.add(i);
-            map.put(nums[i],l);
-        }
-        int min=Integer.MAX_VALUE;
-        for(int key:map.keySet()){
-            if(map.get(key).size()<3) continue;
-            ArrayList<Integer> temp=map.get(key);
-            int i=0;
-            int j=1;
-            int k=2;
-            while(k<temp.size()){
-                min=Math.min(min,2*(Math.max(temp.get(i),Math.max(temp.get(j),temp.get(k)))-Math.min(temp.get(i),Math.min(temp.get(j),temp.get(k)))));
-                i++;
-                j++;
-                k++;
-            }
-        }
-        return min==Integer.MAX_VALUE?-1:min;
-
-    }
-}
+// Last updated: 4/11/2026, 12:43:54 PM
+1class Solution {
+2    public int minimumDistance(int[] nums) {
+3        int n = nums.length;
+4        int ans = Integer.MAX_VALUE;
+5        int prev1[] = new int[n+1];
+6        int prev2[] = new int[n+1];
+7        for(int i=0;i<n+1;i++){
+8            prev1[i] = prev2[i] = -1;
+9        }
+10        
+11        for(int i=0;i<n;i++){
+12            int value = nums[i];
+13            if(prev2[value] != -1){
+14                ans = Math.min(ans, (i-prev2[value]));
+15            }
+16            prev2[value] = prev1[value];
+17            prev1[value] = i;
+18        }
+19        return (ans==Integer.MAX_VALUE)? -1: 2*ans;
+20    }
+21}
