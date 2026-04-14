6bfdf66bbebe3f1432d5b27e2dd93db201d2501b
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-14 20:22:50 +0530

    File updated at 4/14/2026, 8:22:41 PM

--- 1848-MinimumDistancetotheTargetElement/1848-MinimumDistancetotheTargetElement.java
+++ 1848-MinimumDistancetotheTargetElement/1848-MinimumDistancetotheTargetElement.java
@@ -1,4 +1,4 @@
-// Last updated: 4/13/2026, 8:46:51 PM
+// Last updated: 4/14/2026, 8:22:41 PM
 1class Solution {
 2    public int getMinDistance(int[] nums, int target, int start) {
 3        int min=Integer.MAX_VALUE;
