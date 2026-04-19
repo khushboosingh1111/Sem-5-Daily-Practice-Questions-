5e8e246bb4b7964ed34da3ab0987a75d14cfb5d1
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-19 21:22:53 +0530

    Create 1855-MaximumDistanceBetweenaPairofValues

---
+++ 1855-MaximumDistanceBetweenaPairofValues/1855-MaximumDistanceBetweenaPairofValues.java
@@ -0,0 +1,11 @@
+// Last updated: 4/19/2026, 9:22:46 PM
+1class Solution {
+2    public int maxDistance(int[] A, int[] B) {
+3        int i, j = 1;
+4        for (i = 0; i < A.length && j < B.length; j++)
+5            if (A[i] > B[j])
+6                i++;
+7
+8        return j - i - 1;
+9    }
+10}
