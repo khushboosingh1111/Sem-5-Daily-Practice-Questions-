dd4042a53d47d59962e64a2569c5bbb82e13e569
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-20 21:52:36 +0530

    File updated at 4/20/2026, 9:52:35 PM

--- 2078-TwoFurthestHousesWithDifferentColors/2078-TwoFurthestHousesWithDifferentColors.java
+++ 2078-TwoFurthestHousesWithDifferentColors/2078-TwoFurthestHousesWithDifferentColors.java
@@ -1,16 +1,22 @@
-// Last updated: 4/20/2026, 9:52:26 PM
-1class Solution {
-2    public int maxDistance(int[] colors) {
-3        int n = colors.length;
-4        int res = 0; // the maximum distance between two houses of different colors
-5        // traverse the indices of two houses and maintain the maximum distance
-6        for (int i = 0; i < n; i++) {
-7            for (int j = i + 1; j < n; j++) {
-8                if (colors[i] != colors[j]) {
-9                    res = Math.max(res, j - i);
-10                }
-11            }
-12        }
-13        return res;
-14    }
-15}
+// Last updated: 4/20/2026, 9:52:35 PM
+class Solution {
+    public int maxDistance(int[] colors) {
+       int dis = 0;
+       int n = colors.length;
+       for(int i=0;i<n-1;i++){
+        if(colors[i] != colors[n-1]){
+            dis = Math.max(dis ,(n-1)-i);
+            break;
+        }
+       }
+      
+
+       for(int i=n-1;i>=1;i--){
+        if(colors[0]!=colors[i]){
+        dis = Math.max(dis,i);
+       }
+       }
+       return dis;
+    
+    }
+}
