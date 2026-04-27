2c5bd70d9ddc26b821ec39342b2cd9e213702d0b
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-27 22:53:02 +0530

    Create 2053-CheckIfAllCharactersHaveEqualNumberOfOccurrences

---
+++ 2053-CheckIfAllCharactersHaveEqualNumberOfOccurrences/2053-CheckIfAllCharactersHaveEqualNumberOfOccurrences.java
@@ -0,0 +1,18 @@
+// Last updated: 4/27/2026, 10:53:01 PM
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int f=map.get(s.charAt(0));
+        for(char ch:s.toCharArray()){
+            if(f!=(int)map.get(ch)){
+                return false; 
+            }
+
+        }
+        return true;
+        
+    }
+}
