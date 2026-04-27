114e41fcf0b43b6d434d47110e03540272acba37
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-27 22:55:28 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,21 @@
+// Last updated: 4/27/2026, 10:55:27 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        return BS(nums,target,0,nums.length-1);
+    }
+    public int BS(int[] nums,int target,int st,int end){
+        if(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>=target){
+                return BS(nums,target,st,mid-1);
+            }
+            else{
+                return BS(nums,target,mid+1,end);
+            }
+        }
+        return -1;
+    }
+}
