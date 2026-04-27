03f7c51c17492c8c1564533c675c5b9b540296fe
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-27 22:53:59 +0530

    Create 1476-CountNegativeNumbersInASortedMatrix

---
+++ 1476-CountNegativeNumbersInASortedMatrix/1476-CountNegativeNumbersInASortedMatrix.java
@@ -0,0 +1,29 @@
+// Last updated: 4/27/2026, 10:53:57 PM
+class Solution {
+    public int countNegatives(int[][] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            int low = 0;
+            int high = nums[0].length-1;
+            int ans = -1;
+            while (low <= high) {
+
+                int mid = low + (high - low) / 2;
+       
+                if (nums[i][mid] >= 0) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                    ans = mid;
+                }
+
+            }
+            if (ans != -1) {
+                count += ( nums[0].length - ans );
+            }
+        }
+        return count;
+    }
+}
