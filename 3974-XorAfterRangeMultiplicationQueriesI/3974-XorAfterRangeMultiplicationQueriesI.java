72cc03d891b06b93a2a1608fc3fff2e578488297
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-27 22:51:21 +0530

    Create 3974-XorAfterRangeMultiplicationQueriesI

---
+++ 3974-XorAfterRangeMultiplicationQueriesI/3974-XorAfterRangeMultiplicationQueriesI.java
@@ -0,0 +1,21 @@
+// Last updated: 4/27/2026, 10:51:19 PM
+class Solution {
+    private static final int MOD = 1_000_000_007;
+    public int xorAfterQueries(int[] nums, int[][] queries) {
+        int n = nums.length;
+        for (int[] q : queries) {
+            int l = q[0];
+            int r = q[1];
+            int k = q[2];
+            int v = q[3];
+            for (int i = l; i <= r; i += k) {
+                nums[i] = (int) (((long) nums[i] * v) % MOD);
+            }
+        }
+        int res = 0;
+        for (int x : nums) {
+            res ^= x;
+        }
+        return res;
+    }
+}
