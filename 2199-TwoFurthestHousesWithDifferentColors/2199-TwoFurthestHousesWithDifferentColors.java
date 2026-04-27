9df7e074d68ca520541f6e25f9d9de7158a1e75c
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-27 22:52:52 +0530

    Create 2199-TwoFurthestHousesWithDifferentColors

---
+++ 2199-TwoFurthestHousesWithDifferentColors/2199-TwoFurthestHousesWithDifferentColors.java
@@ -0,0 +1,20 @@
+// Last updated: 4/27/2026, 10:52:50 PM
+class Solution {
+    public int maxDistance(int[] colors) {
+       int dis = 0;
+       int n = colors.length;
+       for(int i=0;i<n-1;i++){
+        if(colors[i] != colors[n-1]){
+            dis = Math.max(dis ,(n-1)-i);
+            break;
+        }
+       }
+       for(int i=n-1;i>=1;i--){
+        if(colors[0]!=colors[i]){
+        dis = Math.max(dis,i);
+       }
+       }
+       return dis;
+    
+    }
+}
