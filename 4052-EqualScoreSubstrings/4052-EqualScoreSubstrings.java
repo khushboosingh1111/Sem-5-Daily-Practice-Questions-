4324887d40bf7c3fceb291a5862720ba881cb01c
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-27 22:51:01 +0530

    Create 4052-EqualScoreSubstrings

---
+++ 4052-EqualScoreSubstrings/4052-EqualScoreSubstrings.java
@@ -0,0 +1,23 @@
+// Last updated: 4/27/2026, 10:51:00 PM
+class Solution {
+    public boolean scoreBalance(String s) {
+        int n=s.length();
+        
+        int[] parr=new int[n];
+        parr[0]=s.charAt(0)-'a'+1;
+        for(int i=1;i<n;i++){
+            parr[i]=parr[i-1]+(s.charAt(i)-'a'+1);
+            
+        }
+        int t=parr[n-1];
+        for(int k=0;k<n-1;k++){
+            int lsum=parr[k];
+            int rsum=t-lsum;
+            if(lsum==rsum){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
