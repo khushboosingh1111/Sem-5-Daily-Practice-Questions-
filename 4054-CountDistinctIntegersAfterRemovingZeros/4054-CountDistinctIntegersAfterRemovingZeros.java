9168a2932ca84212538b96e29d182e8f45f80675
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-27 22:51:00 +0530

    Create 4054-CountDistinctIntegersAfterRemovingZeros

---
+++ 4054-CountDistinctIntegersAfterRemovingZeros/4054-CountDistinctIntegersAfterRemovingZeros.java
@@ -0,0 +1,21 @@
+// Last updated: 4/27/2026, 10:50:58 PM
+class Solution {
+    public long countDistinct(long n) {
+        long ans = 0;
+        String s = Long.toString(n);
+        for (int i = 1; i < s.length(); i++) {
+            ans += Math.pow(9, i);
+        }
+        int l = s.length();
+        for (int i = 0; i <= s.length(); i++) {
+            if (i == s.length()) {
+                ans++;
+                break;
+            }
+            long val = s.charAt(i) - '0';
+            if (val == 0) break;
+            ans += (val - 1) * Math.pow(9, l - i - 1);
+        }
+        return ans;
+    }
+}
