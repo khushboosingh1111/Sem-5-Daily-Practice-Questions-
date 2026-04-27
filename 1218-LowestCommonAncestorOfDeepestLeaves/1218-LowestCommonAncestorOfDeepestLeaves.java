dd0b3941b2fa478bd2f22583c271baf90ee176e2
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-27 22:54:21 +0530

    Create 1218-LowestCommonAncestorOfDeepestLeaves

---
+++ 1218-LowestCommonAncestorOfDeepestLeaves/1218-LowestCommonAncestorOfDeepestLeaves.java
@@ -0,0 +1,51 @@
+// Last updated: 4/27/2026, 10:54:19 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+     return dfs(root).node;
+    }
+    
+    private Pair dfs(TreeNode root) {
+        if (root == null) {
+            Pair p = new Pair();
+            p.node = null;
+            p.depth = 0;
+            return p;
+        }
+
+        Pair left = dfs(root.left);
+        Pair right = dfs(root.right);
+
+        Pair result = new Pair();
+        if (left.depth > right.depth) {
+            result.node = left.node;
+            result.depth = left.depth + 1;
+        } else if (right.depth > left.depth) {
+            result.node = right.node;
+            result.depth = right.depth + 1;
+        } else {
+            result.node = root;
+            result.depth = left.depth + 1; // or right.depth + 1 (same)
+        }
+        return result;
+    }
+    
+    private class Pair {
+        TreeNode node;
+        int depth;
+    }
+}
