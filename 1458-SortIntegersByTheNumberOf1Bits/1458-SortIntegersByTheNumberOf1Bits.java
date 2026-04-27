28b27745931ab588f04bf7d6997f3271fa2cc4c2
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-27 22:54:03 +0530

    Create 1458-SortIntegersByTheNumberOf1Bits

---
+++ 1458-SortIntegersByTheNumberOf1Bits/1458-SortIntegersByTheNumberOf1Bits.java
@@ -0,0 +1,31 @@
+// Last updated: 4/27/2026, 10:54:02 PM
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        Integer[] nums = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Comparator<Integer> comparator = new CustomComparator();
+        Arrays.sort(nums, comparator);
+        return Arrays.stream(nums).mapToInt(Integer::intValue).toArray();
+    }
+}
+
+class CustomComparator implements Comparator<Integer> {
+    private int findWeight(int num) {
+        int weight = 0;
+        
+        while (num > 0) {
+            weight++;
+            num &= (num - 1);
+        }
+        
+        return weight;
+    }
+    
+    @Override
+    public int compare(Integer a, Integer b) {
+        if (findWeight(a) == findWeight(b)) {
+            return a - b;
+        }
+        
+        return findWeight(a) - findWeight(b);
+    }
+}
