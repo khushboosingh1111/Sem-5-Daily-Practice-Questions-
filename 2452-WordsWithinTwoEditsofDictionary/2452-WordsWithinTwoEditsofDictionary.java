bef8bebe86b6ad8122f5bc79dcd2029c0caaa1b2
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-27 23:47:01 +0530

    File updated at 4/27/2026, 11:47:00 PM

--- 2452-WordsWithinTwoEditsofDictionary/2452-WordsWithinTwoEditsofDictionary.java
+++ 2452-WordsWithinTwoEditsofDictionary/2452-WordsWithinTwoEditsofDictionary.java
@@ -1,21 +1,28 @@
-// Last updated: 4/27/2026, 11:44:08 PM
+// Last updated: 4/27/2026, 11:47:00 PM
 1class Solution {
 2    public List<String> twoEditWords(String[] queries, String[] dictionary) {
-3        List<String> ans=new ArrayList<>();
-4        for(String q:queries){
-5            for(String d:dictionary){
-6                int edit=0;
-7                for(int i=0;i<q.length();i++){
-8                    if(q.charAt(i)!=d.charAt(i)){
-9                        edit++;
-10                    }
-11                }
-12                if(edit<=2){
-13                    ans.add(q);
-14                    break;
-15                }
-16            }
-17        }
-18        return ans;
-19    }
-20}
+3        List<String> list=new ArrayList<>();
+4        for(String query:queries){
+5            if(solve(query,dictionary)){
+6                list.add(query);
+7            }
+8        }
+9        return list;
+10    }
+11    private boolean solve(String query,String[] dictionary){
+12        for(String dict:dictionary){
+13            int k=0;
+14            for(int i=0;i<dict.length();i++){
+15                    if(dict.charAt(i)!=query.charAt(i)){
+16                        k++;
+17                        if(k>2)break;
+18                        
+19                    }
+20                }
+21                if(k<=2)return true;
+22            
+23        }
+24        return false;
+25    }
+26    
+27}
