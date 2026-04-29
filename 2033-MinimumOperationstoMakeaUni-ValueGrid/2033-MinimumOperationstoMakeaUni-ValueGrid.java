0a1b785f7552e86681856a6422d2e2b5c5f6647f
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-04-29 21:51:35 +0530

    File updated at 4/29/2026, 9:51:26 PM

--- 2033-MinimumOperationstoMakeaUni-ValueGrid/2033-MinimumOperationstoMakeaUni-ValueGrid.java
+++ 2033-MinimumOperationstoMakeaUni-ValueGrid/2033-MinimumOperationstoMakeaUni-ValueGrid.java
@@ -1,20 +1,20 @@
-// Last updated: 4/28/2026, 10:37:24 PM
-class Solution {
-    public int minOperations(int[][] grid, int x) {
-        int m = grid.length, n = grid[0].length;
-        int[] arr = new int[m * n];
-        int k = 0;
-        for (int[] row : grid) {
-            for (int val : row) arr[k++] = val;
-        }
-        int mod = arr[0] % x;
-        for (int val : arr) {
-            if (val % x != mod) return -1;
-        }
-        Arrays.sort(arr);
-        int median = arr[arr.length / 2];
-        long ops = 0;
-        for (int val : arr) ops += Math.abs(val - median) / x;
-        return (int) ops;
-    }
-}
+// Last updated: 4/29/2026, 9:51:26 PM
+1class Solution {
+2    public int minOperations(int[][] grid, int x) {
+3        int m = grid.length, n = grid[0].length;
+4        int[] arr = new int[m * n];
+5        int k = 0;
+6        for (int[] row : grid) {
+7            for (int val : row) arr[k++] = val;
+8        }
+9        int mod = arr[0] % x;
+10        for (int val : arr) {
+11            if (val % x != mod) return -1;
+12        }
+13        Arrays.sort(arr);
+14        int median = arr[arr.length / 2];
+15        long ops = 0;
+16        for (int val : arr) ops += Math.abs(val - median) / x;
+17        return (int) ops;
+18    }
+19}
