653dddce08197b33a0c3b4a52535530344b0e711
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-05-01 00:00:01 +0530

    Create

---
+++ problem-1777573800526/problem-1777573800526.java
@@ -0,0 +1,11 @@
+// Last updated: 5/1/2026, 12:00:00 AM
+1class Solution {
+2    public int findNonMinOrMax(int[] nums) {
+3        Arrays.sort(nums);
+4        int n=nums.length;
+5        if(n==2 || n==1){
+6            return -1;
+7        }
+8        return nums[n/2];
+9    }
+10}
