5283d1d172a7e14778f17c7acc4fb05935d80641
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-05-19 19:33:38 +0530

    File updated at 5/19/2026, 7:33:35 PM

--- 2540-MinimumCommonValue/2540-MinimumCommonValue.java
+++ 2540-MinimumCommonValue/2540-MinimumCommonValue.java
@@ -1,23 +1,23 @@
-// Last updated: 5/19/2026, 7:32:56 PM
-1public class Solution {
+// Last updated: 5/19/2026, 7:33:35 PM
+1class Solution {
 2    public int getCommon(int[] nums1, int[] nums2) {
-3        int first = 0;
-4        int second = 0;
-5
-6        // Traverse through both arrays with two pointers
-7        // Increment the pointer with a smaller value at that index
-8        // Return the first common element found
-9        while (first < nums1.length && second < nums2.length) {
-10            if (nums1[first] < nums2[second]) {
-11                first++;
-12            } else if (nums1[first] > nums2[second]) {
-13                second++;
-14            } else {
-15                return nums1[first];
-16            }
-17        }
-18
-19        // Return -1 if there are no common elements
+3        int i = 0;
+4        int j = 0;
+5        int len1 = nums1.length;
+6        int len2 = nums2.length;
+7        if (nums1[len1 - 1] < nums2[0] || nums2[len2 - 1] < nums1[0]) {
+8            return -1;
+9        }
+10        while (i < len1 && j < len2) {
+11
+12            if (nums1[i] == nums2[j]) {
+13                return nums1[i];
+14            } else if (nums1[i] < nums2[j]) {
+15                i++;
+16            } else {
+17                j++;
+18            }
+19        }
 20        return -1;
 21    }
 22}
