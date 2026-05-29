28df6bc81a3684e60b90d4714ccb4d52cb22212e
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-05-29 21:38:46 +0530

    Create 3300-MinimumElementAfterReplacementWithDigitSum

---
+++ 3300-MinimumElementAfterReplacementWithDigitSum/3300-MinimumElementAfterReplacementWithDigitSum.java
@@ -0,0 +1,16 @@
+// Last updated: 5/29/2026, 9:38:43 PM
+1class Solution {
+2
+3    public int minElement(int[] nums) {
+4        int ans = 37;
+5        for (int num : nums) {
+6            int dig = 0;
+7            while (num > 0) {
+8                dig += num % 10;
+9                num /= 10;
+10            }
+11            ans = Math.min(ans, dig);
+12        }
+13        return ans;
+14    }
+15}
