e357b6ab31a1083b6047c97a0d07e7c1a0794ac0
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-05-31 22:09:21 +0530

    Create 2126-DestroyingAsteroids

---
+++ 2126-DestroyingAsteroids/2126-DestroyingAsteroids.java
@@ -0,0 +1,15 @@
+// Last updated: 5/31/2026, 10:09:16 PM
+1class Solution {
+2    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+3        Arrays.sort(asteroids); // Sort by mass in ascending order
+4        long currentMass = mass; // Preventing integer overflow
+5        for (int asteroid : asteroids) {
+6            // Traverse the asteroids in order, attempt to destroy and update mass or return the result
+7            if (currentMass < asteroid) {
+8                return false;
+9            }
+10            currentMass += asteroid;
+11        }
+12        return true; // Successfully destroy all asteroids
+13    }
+14}
