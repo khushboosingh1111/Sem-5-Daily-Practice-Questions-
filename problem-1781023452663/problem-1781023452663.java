4ff36af297c17482f54b50819162a872af7f584c
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-06-09 22:14:19 +0530

    Create

---
+++ problem-1781023452663/problem-1781023452663.java
@@ -0,0 +1,12 @@
+// Last updated: 6/9/2026, 10:14:12 PM
+1class Solution {
+2    public long maxTotalValue(int[] nums, int k) {
+3        int m1 = Integer.MAX_VALUE;
+4        int m2 = Integer.MIN_VALUE;
+5        for (int x : nums) {
+6            m1 = Math.min(m1, x);
+7            m2 = Math.max(m2, x);
+8        }
+9        return (long) (m2 - m1) * k;
+10    }
+11}
