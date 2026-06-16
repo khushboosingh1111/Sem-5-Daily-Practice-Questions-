dcd35e02d1fd898127d3d55b239c6f48cc50ef73
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-06-17 00:00:08 +0530

    Create 3612-ProcessStringwithSpecialOperationsI

---
+++ 3612-ProcessStringwithSpecialOperationsI/3612-ProcessStringwithSpecialOperationsI.java
@@ -0,0 +1,21 @@
+// Last updated: 6/17/2026, 12:00:01 AM
+1public class Solution {
+2
+3    public String processStr(String s) {
+4        StringBuilder result = new StringBuilder();
+5        for (char ch : s.toCharArray()) {
+6            if (ch == '*') {
+7                if (result.length() > 0) result.deleteCharAt(
+8                    result.length() - 1
+9                );
+10            } else if (ch == '#') {
+11                result.append(result.toString());
+12            } else if (ch == '%') {
+13                result.reverse();
+14            } else {
+15                result.append(ch);
+16            }
+17        }
+18        return result.toString();
+19    }
+20}
