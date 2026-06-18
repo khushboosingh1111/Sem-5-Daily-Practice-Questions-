2409208229ff19519a52b3267af7da7a6f5059cc
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-06-18 21:02:29 +0530

    File updated at 6/18/2026, 9:02:28 PM

--- 1344-AngleBetweenHandsofaClock/1344-AngleBetweenHandsofaClock.java
+++ 1344-AngleBetweenHandsofaClock/1344-AngleBetweenHandsofaClock.java
@@ -1,7 +1,8 @@
-// Last updated: 6/18/2026, 9:02:13 PM
-class Solution {
-    public double angleClock(int hour, int minutes) {
-        double theta = Math.abs(60*hour - 11*minutes)/2.0;
-        return theta > 180.0 ? 360.0-theta : theta;
-    }
-}
+// Last updated: 6/18/2026, 9:02:28 PM
+1class Solution {
+2    public double angleClock(int hour, int minutes) {
+3        double x = hour + minutes / 60.0;
+4        double diff = (11.0 * x) % 12.0;
+5        return Math.min(diff, 12.0 - diff) * 30.0;
+6    }
+7}
