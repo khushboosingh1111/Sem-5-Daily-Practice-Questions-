d9846de7779ac6e9580abbc3d8ca404f65749695
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-06-22 22:45:14 +0530

    File updated at 6/22/2026, 10:45:01 PM

--- 1189-MaximumNumberofBalloons/1189-MaximumNumberofBalloons.java
+++ 1189-MaximumNumberofBalloons/1189-MaximumNumberofBalloons.java
@@ -1,17 +1,19 @@
-// Last updated: 6/22/2026, 10:44:25 PM
+// Last updated: 6/22/2026, 10:45:01 PM
 1class Solution {
-2    public int maxNumberOfBalloons(String s) {
-3        int[] f = new int[5];
-4        String t = "balon";
-5
-6        for (int i = 0; i < s.length(); i++)
-7            for (int j = 0; j < 5; j++)
-8                if (s.charAt(i) == t.charAt(j))
-9                    f[j]++;
-10
-11        f[2] >>= 1;
-12        f[3] >>= 1;
-13
-14        return Arrays.stream(f).min().getAsInt();
-15    }
-16}
+2    public int maxNumberOfBalloons(String text) {
+3        int b_freq = 0;
+4        int a_freq = 0;
+5        int l_freq = 0;
+6        int o_freq = 0;
+7        int n_freq = 0;
+8
+9        for(char ch: text.toCharArray()) {
+10            if(ch == 'b') b_freq++;
+11            else if(ch == 'a') a_freq++;
+12            else if(ch == 'l') l_freq++;
+13            else if(ch == 'o') o_freq++;
+14            else if(ch == 'n') n_freq++;
+15        }
+16        return Math.min(Math.min(b_freq, a_freq), Math.min(n_freq, Math.min(l_freq/2, o_freq/2)));
+17    }
+18}
