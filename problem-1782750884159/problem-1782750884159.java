5cba6b7d1c21c06027f4be80d47b35a965e0e9d2
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-06-29 22:04:46 +0530

    Create

---
+++ problem-1782750884159/problem-1782750884159.java
@@ -0,0 +1,15 @@
+// Last updated: 6/29/2026, 10:04:44 PM
+1class Solution {
+2    public int numOfStrings(String[] patterns, String word) {
+3        int n=patterns.length;
+4        int count=0;
+5        for(int i=0;i<n;i++)
+6        {
+7            if(word.contains(patterns[i]))
+8            {
+9                count++;
+10            }
+11        }
+12        return count;
+13    }
+14}
