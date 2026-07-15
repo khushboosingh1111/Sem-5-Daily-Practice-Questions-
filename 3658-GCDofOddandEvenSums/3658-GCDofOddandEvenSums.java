8370893b5faa97497ab335724721c31d11b79bcf
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-07-15 21:58:40 +0530

    File updated at 7/15/2026, 9:58:35 PM

--- 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.java
+++ 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.java
@@ -1,24 +1,24 @@
-// Last updated: 8/24/2025, 11:14:03 PM
-class Solution {
-    public int gcdOfOddEvenSums(int n) {
-        int so=0;
-        int se=0;
-        for(int i=0;i<=2*n;i++){
-            if(i%2==0){
-                se+=i;
-            }
-            else{
-                so+=i;
-            }
-        }
-        return GCD(se,so);
-    }
-    public int GCD(int se,int so){
-        while(so!=0){
-            int t=so;
-            so=se%so;
-            se=t;
-        }
-        return se;
-    }
-}
+// Last updated: 7/15/2026, 9:58:35 PM
+1class Solution {
+2    public int gcdOfOddEvenSums(int n) {
+3        int so=0;
+4        int se=0;
+5        for(int i=0;i<=2*n;i++){
+6            if(i%2==0){
+7                se+=i;
+8            }
+9            else{
+10                so+=i;
+11            }
+12        }
+13        return GCD(se,so);
+14    }
+15    public int GCD(int se,int so){
+16        while(so!=0){
+17            int t=so;
+18            so=se%so;
+19            se=t;
+20        }
+21        return se;
+22    }
+23}
