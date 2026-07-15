24d8aa679c4580dcbaf652888a218d0eb1466165
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-07-15 22:00:27 +0530

    File updated at 7/15/2026, 10:00:23 PM

--- 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.java
+++ 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.java
@@ -1,8 +1,6 @@
-// Last updated: 7/15/2026, 9:59:40 PM
+// Last updated: 7/15/2026, 10:00:23 PM
 1class Solution {
 2    public int gcdOfOddEvenSums(int n) {
-3        // int odd = n*n;
-4        // int even = n*(n+1);
-5        return n;
-6    }
-7}
+3        return n;
+4    }
+5}
