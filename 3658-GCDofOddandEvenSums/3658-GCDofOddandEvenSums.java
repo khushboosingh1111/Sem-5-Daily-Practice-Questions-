a5ce4d7d55e0eb07587932fce5c4ac6cb7910c87
Khushboo Singh <khushboo2004.singh@gmail.com>
2026-07-15 21:59:45 +0530

    File updated at 7/15/2026, 9:59:40 PM

--- 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.java
+++ 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.java
@@ -1,24 +1,8 @@
-// Last updated: 7/15/2026, 9:58:35 PM
+// Last updated: 7/15/2026, 9:59:40 PM
 1class Solution {
 2    public int gcdOfOddEvenSums(int n) {
-3        int so=0;
-4        int se=0;
-5        for(int i=0;i<=2*n;i++){
-6            if(i%2==0){
-7                se+=i;
-8            }
-9            else{
-10                so+=i;
-11            }
-12        }
-13        return GCD(se,so);
-14    }
-15    public int GCD(int se,int so){
-16        while(so!=0){
-17            int t=so;
-18            so=se%so;
-19            se=t;
-20        }
-21        return se;
-22    }
-23}
+3        // int odd = n*n;
+4        // int even = n*(n+1);
+5        return n;
+6    }
+7}
